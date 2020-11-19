6d03b69c3e93d81d45221b152bf53967e2aa5eca
Pavel Belyaev <zommerberg@abv.bg>
2020-11-19 18:04:15 +0200

    improve main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:boldo/provider/user_provider.dart';
 import 'package:boldo/provider/utils_provider.dart';
-import 'package:boldo/screens/offline/offline_screen.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -10,9 +9,7 @@ import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:dio/dio.dart';
 import 'package:logger/logger.dart';
-import 'package:flutter_appauth/flutter_appauth.dart';
 
 import './network/connection_status.dart';
 import './network/http.dart';
@@ -36,95 +33,9 @@ void main() async {
   connectionStatus.initialize();
   final SharedPreferences prefs = await SharedPreferences.getInstance();
   bool onboardingCompleted = prefs.getBool("onboardingCompleted") ?? false;
+  initDio(navKey: navKey);
   const storage = FlutterSecureStorage();
   String value = await storage.read(key: "access_token");
-
-  String baseUrl = String.fromEnvironment('SERVER_ADDRESS',
-      defaultValue: DotEnv().env['SERVER_ADDRESS']);
-
-  dio.options.baseUrl = baseUrl;
-  dio.options.headers['content-Type'] = 'application/json';
-  dio.options.headers['accept'] = 'application/json';
-
-  String accessToken;
-  FlutterAppAuth appAuth = FlutterAppAuth();
-  //setup interceptors
-  dio.interceptors
-      .add(InterceptorsWrapper(onRequest: (RequestOptions options) async {
-    accessToken ??= await storage.read(key: "access_token");
-    options.headers["authorization"] = "bearer $accessToken";
-
-    return options;
-  }, onError: (DioError error) async {
-    print("base url is ${error.request.baseUrl}");
-    if (error.response?.statusCode == 403) {
-      try {
-        await storage.deleteAll();
-        accessToken = null;
-        navKey.currentState.push(
-          MaterialPageRoute(
-            builder: (context) => DashboardScreen(setLoggedOut: true),
-          ),
-        );
-      } catch (e) {
-        print(e);
-      }
-    } else if (error.response?.statusCode == 401) {
-      RequestOptions options = error.response.request;
-      if ("bearer $accessToken" != options.headers["authorization"]) {
-        options.headers["authorization"] = "bearer $accessToken";
-        return dio.request(options.path, options: options);
-      }
-      dio.lock();
-      dio.interceptors.responseLock.lock();
-      dio.interceptors.errorLock.lock();
-
-      String keycloakRealmAddress = String.fromEnvironment(
-          'KEYCLOAK_REALM_ADDRESS',
-          defaultValue: DotEnv().env['KEYCLOAK_REALM_ADDRESS']);
-      final String refreshToken = await storage.read(key: "refresh_token");
-
-      try {
-        final TokenResponse result = await appAuth.token(TokenRequest(
-            'boldo-patient', 'com.penguin.boldo:/login',
-            discoveryUrl:
-                '$keycloakRealmAddress/.well-known/openid-configuration',
-            refreshToken: refreshToken,
-            scopes: ['openid', 'offline_access']));
-        await storage.write(key: "access_token", value: result.accessToken);
-        await storage.write(key: "refresh_token", value: result.refreshToken);
-        accessToken = result.accessToken;
-        dio.unlock();
-        dio.interceptors.responseLock.unlock();
-        dio.interceptors.errorLock.unlock();
-        //retry request
-        return dio.request(options.path, options: options);
-      } catch (e) {
-        dio.unlock();
-        dio.interceptors.responseLock.unlock();
-        dio.interceptors.errorLock.unlock();
-        await storage.deleteAll();
-        accessToken = null;
-        navKey.currentState.push(
-          MaterialPageRoute(
-            builder: (context) => DashboardScreen(setLoggedOut: true),
-          ),
-        );
-        return error;
-      }
-    }
-    ConnectionStatusSingleton connectionStatus =
-        ConnectionStatusSingleton.getInstance();
-    bool hasInternet = await connectionStatus.checkConnection();
-    if (!hasInternet) {
-      navKey.currentState.push(
-        MaterialPageRoute(
-          builder: (context) => const OfflineScreen(),
-        ),
-      );
-    }
-    return error;
-  }));
 
   runApp(MyApp(
     onboardingCompleted: onboardingCompleted,
--- lib/network/http.dart
+++ lib/network/http.dart
@@ -1,3 +1,105 @@
+import 'package:boldo/network/connection_status.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_appauth/flutter_appauth.dart';
+
+import '../screens/dashboard/dashboard_screen.dart';
+import '../screens/offline/offline_screen.dart';
 
 var dio = Dio();
+
+void initDio({@required GlobalKey<NavigatorState> navKey}) {
+  const storage = FlutterSecureStorage();
+  String baseUrl = String.fromEnvironment('SERVER_ADDRESS',
+      defaultValue: DotEnv().env['SERVER_ADDRESS']);
+
+  dio.options.baseUrl = baseUrl;
+  dio.options.headers['content-Type'] = 'application/json';
+  dio.options.headers['accept'] = 'application/json';
+
+  String accessToken;
+  FlutterAppAuth appAuth = FlutterAppAuth();
+  //setup interceptors
+  dio.interceptors.add(
+    InterceptorsWrapper(
+      onRequest: (RequestOptions options) async {
+        accessToken ??= await storage.read(key: "access_token");
+        options.headers["authorization"] = "bearer $accessToken";
+
+        return options;
+      },
+      onError: (DioError error) async {
+        if (error.response?.statusCode == 403) {
+          try {
+            await storage.deleteAll();
+            accessToken = null;
+            navKey.currentState.push(
+              MaterialPageRoute(
+                builder: (context) => DashboardScreen(setLoggedOut: true),
+              ),
+            );
+          } catch (e) {
+            print(e);
+          }
+        } else if (error.response?.statusCode == 401) {
+          RequestOptions options = error.response.request;
+          if ("bearer $accessToken" != options.headers["authorization"]) {
+            options.headers["authorization"] = "bearer $accessToken";
+            return dio.request(options.path, options: options);
+          }
+          dio.lock();
+          dio.interceptors.responseLock.lock();
+          dio.interceptors.errorLock.lock();
+
+          String keycloakRealmAddress = String.fromEnvironment(
+              'KEYCLOAK_REALM_ADDRESS',
+              defaultValue: DotEnv().env['KEYCLOAK_REALM_ADDRESS']);
+          final String refreshToken = await storage.read(key: "refresh_token");
+
+          try {
+            final TokenResponse result = await appAuth.token(TokenRequest(
+                'boldo-patient', 'com.penguin.boldo:/login',
+                discoveryUrl:
+                    '$keycloakRealmAddress/.well-known/openid-configuration',
+                refreshToken: refreshToken,
+                scopes: ['openid', 'offline_access']));
+            await storage.write(key: "access_token", value: result.accessToken);
+            await storage.write(
+                key: "refresh_token", value: result.refreshToken);
+            accessToken = result.accessToken;
+            dio.unlock();
+            dio.interceptors.responseLock.unlock();
+            dio.interceptors.errorLock.unlock();
+            //retry request
+            return dio.request(options.path, options: options);
+          } catch (e) {
+            dio.unlock();
+            dio.interceptors.responseLock.unlock();
+            dio.interceptors.errorLock.unlock();
+            await storage.deleteAll();
+            accessToken = null;
+            navKey.currentState.push(
+              MaterialPageRoute(
+                builder: (context) => DashboardScreen(setLoggedOut: true),
+              ),
+            );
+            return error;
+          }
+        }
+        ConnectionStatusSingleton connectionStatus =
+            ConnectionStatusSingleton.getInstance();
+        bool hasInternet = await connectionStatus.checkConnection();
+        if (!hasInternet) {
+          navKey.currentState.push(
+            MaterialPageRoute(
+              builder: (context) => const OfflineScreen(),
+            ),
+          );
+        }
+        return error;
+      },
+    ),
+  );
+}
