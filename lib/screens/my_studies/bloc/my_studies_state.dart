b070b900edfc31fc2c96300d0d63419e06bec9af
Joel Epping <joel.sanz14@gmail.com>
2022-06-30 10:14:05 -0400

    feat: Add my studies states

--- lib/screens/my_studies/bloc/my_studies_state.dart
+++ lib/screens/my_studies/bloc/my_studies_state.dart
@@ -4,3 +4,20 @@ part of 'my_studies_bloc.dart';
 abstract class MyStudiesState {}
 
 class MyStudiesInitial extends MyStudiesState {}
+
+class Success extends MyStudiesState {
+  final String nameOfStudies;
+
+  Success({required this.nameOfStudies});
+}
+
+class Loading extends MyStudiesState {}
+
+class Failed extends MyStudiesState {
+  final String msg;
+
+  Failed({required this.msg});
+}
+
+
+
