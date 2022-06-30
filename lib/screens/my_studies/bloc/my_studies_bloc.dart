03a6055f97799bc5795771613832914f7d40eac6
Joel Epping <joel.sanz14@gmail.com>
2022-06-30 10:14:22 -0400

    feat: Manage studies event from bloc

--- lib/screens/my_studies/bloc/my_studies_bloc.dart
+++ lib/screens/my_studies/bloc/my_studies_bloc.dart
@@ -1,32 +1,35 @@
 import 'package:bloc/bloc.dart';
+import 'package:boldo/network/my_studies_repository.dart';
+import 'package:dartz/dartz.dart';
 import 'package:meta/meta.dart';
 
 part 'my_studies_event.dart';
 part 'my_studies_state.dart';
 
 class MyStudiesBloc extends Bloc<MyStudiesEvent, MyStudiesState> {
+  final MyStudesRepository _myStudiesRepository = MyStudesRepository();
   MyStudiesBloc() : super(MyStudiesInitial()) {
-    on<MyStudiesEvent>((event, emit) {
-       if(event is GetPatientStudiesFromServer) {
+    on<MyStudiesEvent>((event, emit) async {
+      if (event is GetPatientStudiesFromServer) {
         print('GetPatientStudiesFromServer capturado');
-        // emit(Loading());
-        // var _post;
-        // await Task(() =>
-        // _patientRepository.getDoctor(id: event.id)!)
-        //     .attempt()
-        //     .run()
-        //     .then((value) {
-        //   _post = value;
-        // }
-        // );
-        // var response;
-        // if (_post.isLeft()) {
-        //   _post.leftMap((l) => response = l.message);
-        //   emit(Failed(response: response));
+        emit(Loading());
+        var _post;
+        await Task(() =>
+        _myStudiesRepository.getPatientStudies()!)
+            .attempt()
+            .run()
+            .then((value) {
+          _post = value;
+        }
+        );
+        var response;
+        if (_post.isLeft()) {
+          _post.leftMap((l) => response = l.message);
+          emit(Failed(msg: response));
 
-        // }else{
-        //   emit(Success());
-        // }
+        }else{
+          emit(Success(nameOfStudies: "Resonancia"));
+        }
       }
     });
   }
