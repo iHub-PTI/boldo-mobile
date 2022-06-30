322f263006709678fbb4ebd7301859da062cb05e
Joel Epping <joel.sanz14@gmail.com>
2022-06-30 10:13:50 -0400

    feat: Add getpatient studies repository

---
+++ lib/network/my_studies_repository.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:boldo/blocs/register_bloc/register_patient_bloc.dart';
+import 'package:boldo/models/MedicalRecord.dart';
+import 'package:boldo/models/Patient.dart';
+import 'package:boldo/network/repository_helper.dart';
+import 'package:camera/camera.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:sentry_flutter/sentry_flutter.dart';
+import '../constants.dart';
+
+import 'http.dart';
+
+class MyStudesRepository {
+  Future<None>? getPatientStudies() async {
+    try {
+
+        await Future.delayed(const Duration(seconds: 2));
+        throw Failure('NO se pudo obtener los datos');
+        // return None();
+      //TODO: agregar endpoint para obtener la lista de estudios
+      // Response response = await dio.get("/profile/patient/diagnosticReports");
+      // if (response.statusCode == 200) {
+      //   // return List<Patient>.from(
+      //   //     response.data.map((i) => Patient.fromJson(i)));
+      // } else if (response.statusCode == 204) {
+      //   return List<Patient>.from([]);
+      // }
+      // throw Failure(genericError);
+    } catch (e) {
+      throw Failure(genericError);
+    }
+  }
+}
