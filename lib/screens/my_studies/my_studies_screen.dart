1de87d3e4f251dddf3c325c0415f5c8ba444a3fa
Joel Epping <joel.sanz14@gmail.com>
2022-07-07 10:20:22 -0400

    feat: Show category label

--- lib/screens/my_studies/my_studies_screen.dart
+++ lib/screens/my_studies/my_studies_screen.dart
@@ -108,23 +108,26 @@ class _MyStudiesState extends State<MyStudies> {
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (BuildContext context) => NewStudy()));
+          Navigator.push(context,
+              MaterialPageRoute(builder: (BuildContext context) => NewStudy()));
         },
         backgroundColor: ConstantsV2.orange,
         label: Container(
             child: Row(
-              children: [
-                Text('nuevo estudio', style: boldoSubTextMediumStyle.copyWith(color: ConstantsV2.lightGrey),),
-                const SizedBox(
-                  width: 8,
-                ),
-                SvgPicture.asset('assets/icon/upload.svg',),
-              ],
-            )
-        ),
+          children: [
+            Text(
+              'nuevo estudio',
+              style: boldoSubTextMediumStyle.copyWith(
+                  color: ConstantsV2.lightGrey),
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            SvgPicture.asset(
+              'assets/icon/upload.svg',
+            ),
+          ],
+        )),
       ),
     );
   }
@@ -147,6 +150,7 @@ class _MyStudiesState extends State<MyStudies> {
   Widget showDiagnosticList() {
     return SizedBox(
       height: MediaQuery.of(context).size.height * 0.7,
+      // width: 300,
       child: ListView.builder(
         itemCount: diagnosticReport.length,
         scrollDirection: Axis.vertical,
@@ -158,148 +162,125 @@ class _MyStudiesState extends State<MyStudies> {
 
   Widget showStudy(BuildContext context, int index) {
     return Card(
+      
       elevation: 4,
       margin: const EdgeInsets.only(bottom: 4),
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) => Study(
+                        id: diagnosticReport[index].id ?? '0',
+                      )));
+        },
         child: Container(
+          
           padding: const EdgeInsets.only(top: 8, left: 8),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Row(
             children: [
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(""),
-                  Container(
-                    margin: const EdgeInsets.only(right: 8),
-                    child: Text(
-                      "${DateFormat('dd/MM/yy').format(DateTime.parse(diagnosticReport[index].effectiveDate!).toLocal())}",
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 7),
+                child: ClipRect(
+                    child: Column(
+                  children: [
+                    SizedBox(
+                      width: 34,
+                      height: 34,
+                      child: SvgPicture.asset(
+                        diagnosticReport[index].type == "LABORATORY"
+                            ? 'assets/icon/lab.svg'
+                            : diagnosticReport[index].type == "IMAGE"
+                                ? 'assets/icon/image.svg'
+                                : diagnosticReport[index].type == "OTHER"
+                                    ? 'assets/icon/other.svg'
+                                    : 'assets/images/LogoIcon.svg',
+                      ),
+                    ),
+                    const SizedBox(height: 3),
+                    Text(
+                      "${diagnosticReport[index].type == "LABORATORY" ? 'lab.' : diagnosticReport[index].type == "IMAGE" ? 'img.' : diagnosticReport[index].type == "OTHER" ? 'otros' : 'desconocido'}",
                       style: boldoCorpSmallTextStyle.copyWith(
                           color: ConstantsV2.darkBlue),
                     ),
-                  )
-                ],
+                  ],
+                )),
               ),
               const SizedBox(
-                height: 10,
+                width: 8,
               ),
-              Row(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 7),
-                    child: ClipRect(
-                        child: Column(
-                      children: [
-                        SizedBox(
-                          width: 34,
-                          height: 34,
-                          child: SvgPicture.asset(
-                            diagnosticReport[index].type == "LABORATORY"
-                                ? 'assets/icon/lab.svg'
-                                : diagnosticReport[index].type == "IMAGE"
-                                    ? 'assets/icon/image.svg'
-                                    : diagnosticReport[index].type == "OTHER"
-                                        ? 'assets/icon/other.svg'
-                                        : 'assets/images/LogoIcon.svg',
-                          ),
-                        ),
-                        Text(
-                          "${diagnosticReport[index].type}",
-                          style: boldoCorpSmallTextStyle.copyWith(
-                              color: ConstantsV2.darkBlue),
-                        ),
-                      ],
-                    )),
-                  ),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          // For a future Laboratory's Name
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        "${diagnosticReport[index].description}",
-                        style: boldoCorpMediumTextStyle.copyWith(
-                            color: ConstantsV2.inactiveText),
-                      ),
-                      Text(
-                        "Subido por:\n${diagnosticReport[index].source}",
-                        style: boldoCorpMediumTextStyle.copyWith(
-                            color: ConstantsV2.inactiveText),
-                      ),
-                      const SizedBox(
-                        height: 4,
-                      ),
-                      Container(
-                        child: Row(
-                          children: [
-                            SvgPicture.asset(
-                              'assets/icon/attach-file.svg',
-                            ),
-                            Text(
-                              "${diagnosticReport[index].attachmentNumber} ${diagnosticReport[index].attachmentNumber == "1" ? "archivo adjunto" : "archivos adjuntos"}",
-                              style: boldoCorpSmallTextStyle.copyWith(
-                                  color: ConstantsV2.darkBlue),
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.end,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Row(
                     mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      /*
-                      Container(
-                        child: GestureDetector(
-                          onTap: () {
-                            // TODO redirect to medical study page
-                          },
-                          child: Card(
-                              margin: EdgeInsets.zero,
-                              clipBehavior: Clip.antiAlias,
-                              elevation: 0,
-                              shape: const RoundedRectangleBorder(
-                                borderRadius: BorderRadius.only(topLeft: Radius.circular(5)),
+                      Card(
+                        shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                        color: Constants.secondaryColor100,
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              left: 10.0, top: 2.0, bottom: 2.0, right: 8.0),
+                          child: Row(
+                            children: [
+                              SvgPicture.asset(
+                                'assets/icon/cloud.svg',
                               ),
-                              color: ConstantsV2.orange.withOpacity(0.10),
-                              child: Container(
-                                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 7),
-                                child: Text("ver archivo"),
-                              )
+                              const SizedBox(width: 6),
+                              Text(
+                                "Boldo",
+                                style: boldoCorpMediumTextStyle.copyWith(
+                                    color: ConstantsV2.inactiveText),
+                              ),
+                            ],
                           ),
                         ),
-                      ),*/
-                      const SizedBox(
-                        height: 14,
                       ),
+                     
+                      Text(
+                        "${DateFormat('dd/MM/yy').format(DateTime.parse(diagnosticReport[index].effectiveDate!).toLocal())}",
+                        style: boldoCorpSmallTextStyle.copyWith(
+                            color: ConstantsV2.darkBlue),
+                      )
                     ],
                   ),
+
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    "${diagnosticReport[index].description}",
+                    style: boldoCorpMediumTextStyle.copyWith(
+                        color: ConstantsV2.inactiveText),
+                  ),
+                  // Text(
+                  //   "Subido por:\n${diagnosticReport[index].source}",
+                  //   style: boldoCorpMediumTextStyle.copyWith(
+                  //       color: ConstantsV2.inactiveText),
+                  // ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Container(
+                    child: Row(
+                      children: [
+                        SvgPicture.asset(
+                          'assets/icon/attach-file.svg',
+                        ),
+                        Text(
+                          "${diagnosticReport[index].attachmentNumber} ${diagnosticReport[index].attachmentNumber == "1" ? "archivo adjunto" : "archivos adjuntos"}",
+                          style: boldoCorpSmallTextStyle.copyWith(
+                              color: ConstantsV2.darkBlue),
+                        )
+                      ],
+                    ),
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ),
