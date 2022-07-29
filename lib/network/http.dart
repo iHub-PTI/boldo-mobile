cd2eea43ae3aed2ddff9cf57c145a6b5b35563df
Ever Fernando Garay Molinas <everfgmolinas@hotmail.com>
2022-07-29 14:03:55 -0400

    chore: remove dioHealthCore

--- lib/network/http.dart
+++ lib/network/http.dart
@@ -13,7 +13,6 @@ import '../screens/dashboard/dashboard_screen.dart';
 import '../screens/offline/offline_screen.dart';
 
 var dio = Dio();
-var dioHealthCore = Dio();
 void initDio({required GlobalKey<NavigatorState> navKey}) {
   String baseUrl = String.fromEnvironment('SERVER_ADDRESS',
       defaultValue: dotenv.env['SERVER_ADDRESS']!);
@@ -103,7 +102,7 @@ void initDio({required GlobalKey<NavigatorState> navKey}) {
               data: options.data, options: optionsDio));
         } on DioError catch(exception){
           if (exception.response?.statusCode == 401){
-            final _result = await authenticateUser();
+            final _result = await authenticateUser(context: navKey.currentState!.context);
             switch (_result) {
               case 0:
                 //user canceled or generic error
@@ -180,127 +179,8 @@ void initDio({required GlobalKey<NavigatorState> navKey}) {
             responseType: options.responseType,
             sendTimeout: options.sendTimeout,
             validateStatus: options.validateStatus);
-        return handle.resolve(await dioHealthCore.request(options.path,
+        return handle.resolve(await dio.request(options.path,
             data: options.data, options: optionsDio));
-      }
-      return handle.next(error);
-    },
-  ));
-}
-
-void initDioSecondaryAccess({required GlobalKey<NavigatorState> navKey}) {
-  String baseUrl = String.fromEnvironment('HEALTH_PTI_API',
-      defaultValue: dotenv.env['HEALTH_PTI_API']!);
-
-  dioHealthCore.options.baseUrl = baseUrl;
-  dioHealthCore.options.headers['content-Type'] = 'application/json';
-  dioHealthCore.options.headers['accept'] = 'application/json';
-  // dioHealthCore.options.connectTimeout = 5000; //5s
-  // dioHealthCore.options.receiveTimeout = 3000;
-  String? accessToken;
-  FlutterAppAuth appAuth = FlutterAppAuth();
-  //setup interceptors
-
-  dioHealthCore.interceptors.add(QueuedInterceptorsWrapper(
-    onRequest: (options, handler) async {
-      accessToken = (await storage.read(key: "access_token"))!;
-      options.headers["authorization"] = "bearer $accessToken";
-
-      return handler.next(options);
-    },
-    onError: (DioError error, handle) async {
-      if (error.response?.statusCode == 403) {
-        try {
-          await storage.deleteAll();
-          navKey.currentState!.pushAndRemoveUntil(
-            MaterialPageRoute(
-              builder: (context) => DashboardScreen(setLoggedOut: true),
-            ),
-            (route) => false,
-          );
-          // ignore: null_check_always_fails
-          accessToken = null!;
-        } catch (e) {
-          print(e);
-        }
-      } else if (error.response?.statusCode == 401) {
-        print("401 DIOHealtcore");
-        if (accessToken == null) {
-          await storage.deleteAll();
-
-          return handle.next(error);
-        }
-
-        RequestOptions options = error.response!.requestOptions;
-        Options optionsDio = Options(
-            contentType: options.contentType,
-            followRedirects: options.followRedirects,
-            extra: options.extra,
-            headers: options.headers,
-            listFormat: options.listFormat,
-            maxRedirects: options.maxRedirects,
-            method: options.method,
-            receiveDataWhenStatusError: options.receiveDataWhenStatusError,
-            receiveTimeout: options.receiveTimeout,
-            requestEncoder: options.requestEncoder,
-            responseDecoder: options.responseDecoder,
-            responseType: options.responseType,
-            sendTimeout: options.sendTimeout,
-            validateStatus: options.validateStatus);
-        if ("bearer $accessToken" != options.headers["authorization"]) {
-          options.headers["authorization"] = "bearer $accessToken";
-          handle.resolve(
-              await dioHealthCore.request(options.path, options: optionsDio));
-        }
-        dioHealthCore.lock();
-        dioHealthCore.interceptors.responseLock.lock();
-        dioHealthCore.interceptors.errorLock.lock();
-
-        String keycloakRealmAddress = String.fromEnvironment(
-            'KEYCLOAK_REALM_ADDRESS',
-            defaultValue: dotenv.env['KEYCLOAK_REALM_ADDRESS']!);
-        final String? refreshToken = await storage.read(key: "refresh_token");
-
-        try {
-          final TokenResponse? result = await appAuth.token(TokenRequest(
-              'boldo-patient', 'py.org.pti.boldo:/login',
-              discoveryUrl:
-                  '$keycloakRealmAddress/.well-known/openid-configuration',
-              refreshToken: refreshToken,
-              scopes: ['openid', 'offline_access']));
-          await storage.write(key: "access_token", value: result!.accessToken);
-          await storage.write(key: "refresh_token", value: result.refreshToken);
-          accessToken = result.accessToken;
-          dioHealthCore.unlock();
-          dioHealthCore.interceptors.responseLock.unlock();
-          dioHealthCore.interceptors.errorLock.unlock();
-          return handle.resolve(await dioHealthCore.request(options.path,
-              data: options.data, options: optionsDio));
-        } catch (e) {
-          dioHealthCore.unlock();
-          dioHealthCore.interceptors.responseLock.unlock();
-          dioHealthCore.interceptors.errorLock.unlock();
-          await storage.deleteAll();
-          accessToken = null;
-
-          navKey.currentState!.pushAndRemoveUntil(
-            MaterialPageRoute(
-              builder: (context) => DashboardScreen(setLoggedOut: true),
-            ),
-            (route) => false,
-          );
-          return handle.next(error);
-        }
-      }
-      ConnectionStatusSingleton connectionStatus =
-          ConnectionStatusSingleton.getInstance();
-      bool hasInternet = await connectionStatus.checkConnection();
-      if (!hasInternet) {
-        navKey.currentState!.push(
-          MaterialPageRoute(
-            builder: (context) => const OfflineScreen(),
-          ),
-        );
       }
       return handle.next(error);
     },
