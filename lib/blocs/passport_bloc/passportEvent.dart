4885478f3c34814f6cedeb4ca4f13e85043e962a
elias7991 <78571779+elias7991@users.noreply.github.com>
2022-09-21 16:57:09 -0400

    feat: event for download pdf

--- lib/blocs/passport_bloc/passportEvent.dart
+++ lib/blocs/passport_bloc/passportEvent.dart
@@ -6,3 +6,8 @@ abstract class PassportEvent {}
 class GetUserDiseaseList extends PassportEvent {}
 
 class GetUserDiseaseListSync extends PassportEvent {}
+
+class GetUserVaccinationPdfPressed extends PassportEvent {
+  bool pdfFromHome;
+  GetUserVaccinationPdfPressed({required this.pdfFromHome});
+}
