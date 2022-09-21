86dca2e01a3a1094134c4da8f7af709495b3cc99
elias7991 <78571779+elias7991@users.noreply.github.com>
2022-09-21 14:17:27 -0400

    feat: query for vaccine list

--- lib/network/passport_repository.dart
+++ lib/network/passport_repository.dart
@@ -1,47 +1,46 @@
 // get vaccine
 import 'dart:convert';
 
+import 'package:boldo/network/http.dart';
 import 'package:boldo/network/repository_helper.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 import '../main.dart';
 import '../models/UserVaccinate.dart';
 
 class PassportRepository {
-  Future<None>? getUserDiseaseList() async {
+  Future<None>? getUserDiseaseList(bool needSync) async {
     try {
       print('get user Disease List');
       //fake json Covid
-      var covidFake = await rootBundle.loadString('assets/fake_jsons/covid_vaccine.json');
-      final data = await json.decode(covidFake);
-      var covidVaccinate = userVaccinateFromJson(data);
-
-      //fake json Influenza
-      var influenzaFake = await rootBundle.loadString('assets/fake_jsons/influenza_vaccine.json');
-      final influenzaData = await json.decode(influenzaFake);
-      var influenzaVaccinate = userVaccinateFromJson(influenzaData);
-
-      //fake json Varicela
-      var varicelaFake = await rootBundle.loadString('assets/fake_jsons/varicela_vaccine.json');
-      final varicelaData = await json.decode(varicelaFake);
-      var varicelaVaccinate = userVaccinateFromJson(varicelaData);
-
-      diseaseUserList = [influenzaVaccinate,varicelaVaccinate,covidVaccinate];
-
+      Response response = await dioPassport.get(
+          '/profile/citizen/vaccinationRegistry/list?all=true&sync=$needSync&groupByDisease=false');
+      if (response.statusCode == 200) {
+        diseaseUserList = userVaccinateFromJson(response.data);
+      }
       return None();
-
-    } catch (e) {
-      print(e);
-      if (e is DioError) {
-        //handle DioError here by error type or by error code
-
-        if (e.error.source != null) {
-          throw Failure('${e.error.source}');
-        }
-      } 
-       throw Failure('Error genérico');
+    } on DioError catch (e) {
+      await Sentry.captureMessage(
+        e.toString(),
+        params: [
+          {
+            "path": e.requestOptions.path,
+            "data": e.requestOptions.data,
+            "patient": patient.id,
+            "responseError": e.response,
+          }
+        ],
+      );
+      throw Failure(e.response?.data['message']);
+    } catch (exception, stackTrace) {
+      await Sentry.captureException(
+        exception,
+        stackTrace: stackTrace,
+      );
+      throw Failure('Ocurrio un error indesperado');
     }
   }
 }
