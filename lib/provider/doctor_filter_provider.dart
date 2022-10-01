640ddaa40b42842f123204fdb0d08b8b09dd11b6
elias7991 <78571779+elias7991@users.noreply.github.com>
2022-09-30 20:12:04 -0400

    feat: add doctor filter provider

---
+++ lib/provider/doctor_filter_provider.dart
@@ -0,0 +1,62 @@
+import 'package:boldo/models/Doctor.dart';
+import 'package:flutter/foundation.dart';
+
+class DoctorFilterProvider with ChangeNotifier {
+  // this is a list of specializations was selected in filter
+  List<Specializations> _selectedSpecializations = [];
+  // these are a boolean variables to check the type of appointment
+  bool virtualAppointment = false;
+  bool inPersonAppointment = false;
+
+  // functions to get the variables
+
+  // get filter state
+  bool get getFilterState =>
+      virtualAppointment ||
+      inPersonAppointment ||
+      _selectedSpecializations.isNotEmpty;
+
+  // get specializations
+  List<Specializations> get getSpecializations => _selectedSpecializations;
+
+  // set list of specializations
+  void setListOfSpecializations(
+      {required List<Specializations> selectedFilters}) {
+    _selectedSpecializations = selectedFilters;
+    // call bloc event
+
+    notifyListeners();
+  }
+
+  // delete specializations
+  void removeSpecialization({required String specializationId}) {
+    _selectedSpecializations = _selectedSpecializations
+        .where((element) => element.id != specializationId)
+        .toList();
+    // call bloc event
+
+    notifyListeners();
+  }
+
+  // get true if the appointment is virtual
+  bool get getVirtualAppointment => virtualAppointment;
+
+  // set true or false the virtual appointment
+  void setVirtualAppointment() {
+    virtualAppointment = !virtualAppointment;
+    // call bloc event
+
+    notifyListeners();
+  }
+
+  // get true if the appointment is in person
+  bool get getInPersonAppointment => inPersonAppointment;
+
+  // set true or false the in person appointment
+  void setInPersonAppointment() {
+    inPersonAppointment = !inPersonAppointment;
+    // call bloc event
+
+    notifyListeners();
+  }
+}
