778e07d809de215ae0a2ee5852bacdadfe2fe36a
elias7991 <78571779+elias7991@users.noreply.github.com>
2022-10-07 22:27:16 -0300

    feat: event to reload filter

--- lib/blocs/doctors_available_bloc/doctors_available_event.dart
+++ lib/blocs/doctors_available_bloc/doctors_available_event.dart
@@ -3,7 +3,19 @@ part of 'doctors_available_bloc.dart';
 @immutable
 abstract class DoctorsAvailableEvent {}
 
-
 class GetDoctorsAvailable extends DoctorsAvailableEvent {}
+
+class GetDoctorFilter extends DoctorsAvailableEvent {
+  final List<Specializations> specializations;
+  final bool virtualAppointment;
+  final bool inPersonAppointment;
+  GetDoctorFilter({
+    required this.specializations,
+    required this.virtualAppointment,
+    required this.inPersonAppointment
+  });
+}
+
 class GetSpecializations extends DoctorsAvailableEvent {}
+
 class ReloadDoctorsAvailable extends DoctorsAvailableEvent {}
