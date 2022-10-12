35b1063cded251615b1fa98e72c115a29b65b825
elias7991 <78571779+elias7991@users.noreply.github.com>
2022-10-11 21:51:42 -0300

    ref: query for doctor filter

--- lib/network/doctor_repository.dart
+++ lib/network/doctor_repository.dart
@@ -1,8 +1,11 @@
+import 'package:boldo/constants.dart';
+import 'package:boldo/main.dart';
 import 'package:boldo/models/Doctor.dart';
 import 'package:boldo/network/http.dart';
 import 'package:boldo/network/repository_helper.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 class DoctorRepository {
   Future<List<Doctor>>? getAllDoctors() async {
@@ -21,67 +24,47 @@ class DoctorRepository {
   Future<List<Doctor>> getDoctorsFilter(List<Specializations> specializations,
       bool virtualAppointment, bool inPersonAppointment) async {
     try {
-      // 173 is the complete list of specializations
-      Response response;
-      if (specializations.length == 173 &&
-          virtualAppointment &&
-          inPersonAppointment) {
-        response = await dio.get('/doctors');
-        if (response.statusCode == 200) {
-          return List<Doctor>.from(
-              response.data['items'].map((i) => Doctor.fromJson(i)));
+      // list of IDs
+      List<String> listOfSpecializations =
+          specializations.map((e) => e.id!).toList();
+      String appointmentType = "";
+      // here set the tipe of appointment
+      if (virtualAppointment && inPersonAppointment) {
+        appointmentType = "AV";
+      } else if (virtualAppointment) {
+        appointmentType = "V";
+      } else if (inPersonAppointment) {
+        appointmentType = "A";
+      }
+      Response response = await dio.get('/doctors',
+        queryParameters: {
+          "appointmentType": appointmentType,
+          "specialtyIds": listOfSpecializations
         }
-      } else if (specializations.length != 0 && specializations.length != 173) {
-        // list of IDs
-        List<String> listOfSpecializations =
-            specializations.map((e) => e.id!).toList();
-        String appointmentType = "";
-        if (virtualAppointment && inPersonAppointment) {
-          appointmentType = "AV";
-        } else if (virtualAppointment) {
-          appointmentType = "V";
-        } else if (inPersonAppointment) {
-          appointmentType = "A";
-        }
-
-        if (appointmentType != "") {
-          response = await dio.get('/doctors', queryParameters: {
-            "appointmentType": appointmentType,
-            "specialtyIds": listOfSpecializations
-          });
-          if (response.statusCode == 200) {
-            return List<Doctor>.from(
-                response.data['items'].map((i) => Doctor.fromJson(i)));
-          }
-        } else {
-          response = await dio.get('/doctors',
-              queryParameters: {"specialtyIds": listOfSpecializations});
-          if (response.statusCode == 200) {
-            return List<Doctor>.from(
-                response.data['items'].map((i) => Doctor.fromJson(i)));
-          }
-        }
-      } else if (specializations.isEmpty &&
-          (virtualAppointment || inPersonAppointment)) {
-        String appointmentType = "";
-        if (virtualAppointment && inPersonAppointment) {
-          appointmentType = "AV";
-        } else if (virtualAppointment) {
-          appointmentType = "V";
-        } else if (inPersonAppointment) {
-          appointmentType = "A";
-        }
-        response = await dio.get('/doctors', queryParameters: {
-          "appointmentType": appointmentType
-        });
-        if (response.statusCode == 200) {
-          return List<Doctor>.from(
-              response.data['items'].map((i) => Doctor.fromJson(i)));
-        }
+      );
+      if (response.statusCode == 200) {
+        return List<Doctor>.from(response.data['items'].map((i) => Doctor.fromJson(i)));
       }
       throw Failure('No se pudo obtener la lista de médicos');
-    } catch (e) {
+    } on DioError catch(ex) {
+      await Sentry.captureMessage(
+        ex.toString(),
+        params: [
+          {
+            "path": ex.requestOptions.path,
+            "data": ex.requestOptions.data,
+            "patient": prefs.getString("userId"),
+            "responseError": ex.response,
+          }
+        ],
+      );
       throw Failure('No se pudo obtener la lista de médicos');
+    } catch (exception, stackTrace) {
+      await Sentry.captureException(
+        exception,
+        stackTrace: stackTrace,
+      );
+      throw Failure(genericError);
     }
   }
 
