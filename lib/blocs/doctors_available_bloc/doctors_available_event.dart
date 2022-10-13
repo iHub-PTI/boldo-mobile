0ff01678cb1be1098509777667f695800bb5fa67
elias7991 <78571779+elias7991@users.noreply.github.com>
2022-10-13 15:48:54 -0300

    feat: event for get more doctors

--- lib/blocs/doctors_available_bloc/doctors_available_event.dart
+++ lib/blocs/doctors_available_bloc/doctors_available_event.dart
@@ -3,17 +3,24 @@ part of 'doctors_available_bloc.dart';
 @immutable
 abstract class DoctorsAvailableEvent {}
 
-class GetDoctorsAvailable extends DoctorsAvailableEvent {}
+class GetDoctorsAvailable extends DoctorsAvailableEvent {
+  final int offset;
+  GetDoctorsAvailable({required this.offset});
+}
+
+class GetMoreDoctorsAvailable extends DoctorsAvailableEvent {
+  final int offset;
+  GetMoreDoctorsAvailable({required this.offset});
+}
 
 class GetDoctorFilter extends DoctorsAvailableEvent {
   final List<Specializations> specializations;
   final bool virtualAppointment;
   final bool inPersonAppointment;
-  GetDoctorFilter({
-    required this.specializations,
-    required this.virtualAppointment,
-    required this.inPersonAppointment
-  });
+  GetDoctorFilter(
+      {required this.specializations,
+      required this.virtualAppointment,
+      required this.inPersonAppointment});
 }
 
 class GetSpecializations extends DoctorsAvailableEvent {}
