cbadbf57cf689491bcb5ecc19a276ddf40869f69
elias7991 <78571779+elias7991@users.noreply.github.com>
2022-11-03 20:32:00 -0300

    feat: states to control relation load

--- lib/blocs/family_bloc/dependent_family_state.dart
+++ lib/blocs/family_bloc/dependent_family_state.dart
@@ -1,12 +1,13 @@
 part of 'dependent_family_bloc.dart';
 
 @immutable
-abstract class FamilyState{}
+abstract class FamilyState {}
 
 class FamilyInitial extends FamilyState {}
 
 class Loading extends FamilyState {}
 
+class RelationLoading extends FamilyState {}
 class Failed extends FamilyState {
   final response;
   Failed({required this.response});
@@ -22,3 +23,5 @@ class CaretakersObtained extends FamilyState {
   final List<Patient> caretakers;
   CaretakersObtained({required this.caretakers});
 }
+
+class RelationSuccess extends FamilyState {}
