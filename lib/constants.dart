a5ff6e1678cc6b1a7e4ea33c950c910a73a32d4c
Ever Garay <everfgmolinas@hotmail.com>
2022-11-17 09:01:22 -0300

    style: define success dependent added message

--- lib/constants.dart
+++ lib/constants.dart
@@ -412,3 +412,4 @@ class ConstantsV2 {
 const String isFamily = 'isFamily';
 const int minutesToCloseAppointment = 120;
 const String uploadedStudySuccessfullyMessage = "¡Estudio subido!";
+const String dependentSuccessAdded = '¡Dependiente agregado!';
