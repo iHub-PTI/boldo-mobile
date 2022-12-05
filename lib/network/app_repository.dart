3ba59be8cb82650f4983aa9d20638790c5f04c02
Ever Garay <everfgmolinas@hotmail.com>
2022-12-05 13:31:57 -0300

    feat: create a class for App Repository

--- lib/network/app_repository.dart
+++ lib/network/app_repository.dart
@@ -7,70 +7,74 @@ import 'package:dio/dio.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
-Future<bool> validateAppVersion() async {
-  try {
-    String? backendVersion;
-    // get app version from server
-    if(Platform.isAndroid){
-      // Get version from server for Android
-      backendVersion = '1.2.7';
-    }else if(Platform.isIOS){
-      // Get version from server for IOS
-      backendVersion = '1.1.8';
-    }else{
-      throw Failure("Unkown SO ${Platform.operatingSystem}");
+class AppRepository{
+
+  Future<bool> validateAppVersion() async {
+    try {
+      String? backendVersion;
+      // get app version from server
+      if(Platform.isAndroid){
+        // Get version from server for Android
+        backendVersion = '1.2.7';
+      }else if(Platform.isIOS){
+        // Get version from server for IOS
+        backendVersion = '1.1.8';
+      }else{
+        throw Failure("Unkown SO ${Platform.operatingSystem}");
+      }
+
+
+      // get package info
+      PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+      //validate locale version with server version
+      // negative if the version from server is bigger than device version
+      int validation = packageInfo.version.compareTo(backendVersion);
+
+      return validation < 0;
+    } on Failure catch (exception, stackTrace) {
+      await Sentry.captureMessage(
+          exception.toString(),
+          params: [
+            {
+              'responseError': exception,
+              'patient': prefs.getString("userId"),
+              'access_token': await storage.read(key: 'access_token')
+            },
+            stackTrace
+          ]
+      );
+      throw Failure(genericError);
+    } on DioError catch (exception, stackTrace) {
+      await Sentry.captureMessage(
+        exception.toString(),
+        params: [
+          {
+            "path": exception.requestOptions.path,
+            "data": exception.requestOptions.data,
+            "patient": prefs.getString("userId"),
+            "dependentId": patient.id,
+            "responseError": exception.response,
+            'access_token': await storage.read(key: 'access_token')
+          },
+          stackTrace
+        ],
+      );
+      throw Failure(genericError);
+    } catch (exception, stackTrace) {
+      await Sentry.captureMessage(
+          exception.toString(),
+          params: [
+            {
+              'responseError': exception,
+              'patient': prefs.getString("userId"),
+              'access_token': await storage.read(key: 'access_token')
+            },
+            stackTrace
+          ]
+      );
+      throw Failure(genericError);
     }
-
-
-    // get package info
-    PackageInfo packageInfo = await PackageInfo.fromPlatform();
-
-    //validate locale version with server version
-    // negative if the version from server is bigger than device version
-    int validation = packageInfo.version.compareTo(backendVersion);
-
-    return validation < 0;
-  } on Failure catch (exception, stackTrace) {
-    await Sentry.captureMessage(
-        exception.toString(),
-        params: [
-          {
-            'responseError': exception,
-            'patient': prefs.getString("userId"),
-            'access_token': await storage.read(key: 'access_token')
-          },
-          stackTrace
-        ]
-    );
-    throw Failure(genericError);
-  } on DioError catch (exception, stackTrace) {
-    await Sentry.captureMessage(
-      exception.toString(),
-      params: [
-        {
-          "path": exception.requestOptions.path,
-          "data": exception.requestOptions.data,
-          "patient": prefs.getString("userId"),
-          "dependentId": patient.id,
-          "responseError": exception.response,
-          'access_token': await storage.read(key: 'access_token')
-        },
-        stackTrace
-      ],
-    );
-    throw Failure(genericError);
-  } catch (exception, stackTrace) {
-    await Sentry.captureMessage(
-        exception.toString(),
-        params: [
-          {
-            'responseError': exception,
-            'patient': prefs.getString("userId"),
-            'access_token': await storage.read(key: 'access_token')
-          },
-          stackTrace
-        ]
-    );
-    throw Failure(genericError);
   }
+
 }
