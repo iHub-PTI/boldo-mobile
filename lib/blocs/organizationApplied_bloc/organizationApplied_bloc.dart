4d9b73a9e90f3a9cae0caf5f12ff18c53220f4aa
Ever Garay <everfgmolinas@hotmail.com>
2022-12-21 08:18:24 -0300

    feat: block to get the organizations to which the patient applied

---
+++ lib/blocs/organizationApplied_bloc/organizationApplied_bloc.dart
@@ -0,0 +1,70 @@
+import 'package:boldo/models/Organization.dart';
+import 'package:boldo/models/QRCode.dart';
+import 'package:boldo/network/organization_repository.dart';
+import 'package:boldo/network/user_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../main.dart';
+
+
+part 'organizationApplied_event.dart';
+part 'organizationApplied_state.dart';
+
+class OrganizationAppliedBloc extends Bloc<OrganizationAppliedBlocEvent, OrganizationAppliedBlocState> {
+  final OrganizationRepository _organizationRepository = OrganizationRepository();
+  OrganizationAppliedBloc() : super(OrganizationInitialState()) {
+    on<OrganizationAppliedBlocEvent>((event, emit) async {
+      if(event is GetOrganizationsPostulated) {
+        emit(Loading());
+        var _post;
+
+        //get organizations that the patient is subscribed
+        await Task(() =>
+        _organizationRepository.getPostulatedOrganizations()!)
+            .attempt()
+            .run()
+            .then((value) {
+          _post = value;
+        }
+        );
+        var response;
+        if (_post.isLeft()) {
+          _post.leftMap((l) => response = l.message);
+          emit(Failed(response: response));
+
+        }else{
+          late List<Organization> organizations;
+          _post.foldRight(QRCode, (a, previous) => organizations = a);
+
+          emit(OrganizationsObtained(organizationsList: organizations));
+        }
+      }else if(event is UnPostulated) {
+        emit(Loading());
+        var _post;
+
+        //unsubscribed to an one organization
+        await Task(() =>
+        _organizationRepository.unSubscribedPostulation(event.id)!)
+            .attempt()
+            .run()
+            .then((value) {
+          _post = value;
+        }
+        );
+        var response;
+        if (_post.isLeft()) {
+          _post.leftMap((l) => response = l.message);
+          emit(Failed(response: response));
+
+        }else{
+
+          emit(PostulationRemoved(id: event.id));
+        }
+      }
+    }
+
+    );
+  }
+}
