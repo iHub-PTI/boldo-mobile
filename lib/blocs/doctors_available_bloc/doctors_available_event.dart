1d0c57a76118b31db12cf92b743e954bdcd3bd66
Ever Garay <everfgmolinas@hotmail.com>
2023-01-17 18:29:46 -0300

    refactor: get doctors by organization

--- lib/blocs/doctors_available_bloc/doctors_available_event.dart
+++ lib/blocs/doctors_available_bloc/doctors_available_event.dart
@@ -5,15 +5,24 @@ abstract class DoctorsAvailableEvent {}
 
 class GetDoctorsAvailable extends DoctorsAvailableEvent {
   final int offset;
-  GetDoctorsAvailable({required this.offset});
+  final List<Organization> organizations;
+  GetDoctorsAvailable({
+    required this.offset,
+    required this.organizations,
+  });
 }
 
 class GetMoreDoctorsAvailable extends DoctorsAvailableEvent {
   final int offset;
-  GetMoreDoctorsAvailable({required this.offset});
+  final List<Organization> organizations;
+  GetMoreDoctorsAvailable({
+    required this.offset,
+    required this.organizations,
+  });
 }
 
 class GetDoctorFilter extends DoctorsAvailableEvent {
+  final List<Organization> organizations;
   final List<Specializations> specializations;
   final bool virtualAppointment;
   final bool inPersonAppointment;
@@ -21,32 +30,36 @@ class GetDoctorFilter extends DoctorsAvailableEvent {
     required this.specializations,
     required this.virtualAppointment,
     required this.inPersonAppointment,
+    required this.organizations,
   });
 }
 
 class GetDoctorFilterInDoctorList extends DoctorsAvailableEvent {
+  final List<Organization> organizations;
   final List<Specializations> specializations;
   final bool virtualAppointment;
   final bool inPersonAppointment;
   GetDoctorFilterInDoctorList({
     required this.specializations,
     required this.virtualAppointment,
-    required this.inPersonAppointment
+    required this.inPersonAppointment,
+    required this.organizations,
   });
 }
 
 class GetMoreFilterDoctor extends DoctorsAvailableEvent {
+  final List<Organization> organizations;
   final int offset;
   final List<Specializations> specializations;
   final bool virtualAppointment;
   final bool inPersonAppointment;
-  GetMoreFilterDoctor(
-      {required this.offset,
-      required this.specializations,
-      required this.virtualAppointment,
-      required this.inPersonAppointment});
+  GetMoreFilterDoctor({
+    required this.offset,
+    required this.specializations,
+    required this.virtualAppointment,
+    required this.inPersonAppointment,
+    required this.organizations
+  });
 }
-
-class GetSpecializations extends DoctorsAvailableEvent {}
 
 class ReloadDoctorsAvailable extends DoctorsAvailableEvent {}
