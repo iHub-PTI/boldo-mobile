42f0977226652239cdaff109fad393c0f3a01f93
Ever Garay <everfgmolinas@hotmail.com>
2023-01-17 18:28:02 -0300

    refactor: sort doctors by organization and nextAvailability

--- lib/blocs/doctors_available_bloc/doctors_available_bloc.dart
+++ lib/blocs/doctors_available_bloc/doctors_available_bloc.dart
@@ -1,5 +1,7 @@
+import 'package:boldo/blocs/user_bloc/patient_bloc.dart';
 import 'package:boldo/main.dart';
 import 'package:boldo/models/Doctor.dart';
+import 'package:boldo/models/Organization.dart';
 import 'package:boldo/network/doctor_repository.dart';
 import 'package:boldo/provider/doctor_filter_provider.dart';
 import 'package:dartz/dartz.dart';
@@ -57,7 +59,8 @@ class DoctorsAvailableBloc
           event.offset,
           event.specializations,
           event.virtualAppointment,
-          event.inPersonAppointment))
+          event.inPersonAppointment,
+          event.organizations))
             .attempt()
             .run()
             .then((value) {
@@ -78,26 +81,6 @@ class DoctorsAvailableBloc
         emit(FilterLoaded(doctors: Provider.of<DoctorFilterProvider>(navKey.currentState!.context, listen: false)
             .getDoctorsSaved));
         emit(FilterSucces());
-      } else if (event is GetSpecializations) {
-        emit(Loading());
-        var _post;
-        await Task(() => _doctorRepository.getAllSpecializations()!)
-            .attempt()
-            .run()
-            .then((value) {
-          _post = value;
-        });
-        var response;
-        if (_post.isLeft()) {
-          _post.leftMap((l) => response = l.message);
-          emit(Failed(response: response));
-        } else {
-          late List<Specializations> specializations = [];
-          _post.foldRight(
-              Specializations, (a, previous) => specializations = a);
-          emit(SpecializationsLoaded(specializations: specializations));
-          emit(Success());
-        }
       } else if (event is GetDoctorFilter) {
         emit(FilterLoading());
         var _post;
@@ -105,7 +88,8 @@ class DoctorsAvailableBloc
             0,
             event.specializations,
             event.virtualAppointment,
-            event.inPersonAppointment)).attempt().run().then((value) {
+            event.inPersonAppointment,
+            event.organizations)).attempt().run().then((value) {
           _post = value;
         });
         var response;
@@ -115,7 +99,18 @@ class DoctorsAvailableBloc
         } else {
           late List<Doctor> doctors = [];
           _post.foldRight(Doctor, (a, previous) => doctors = a);
-          emit(FilterLoaded(doctors: doctors));
+
+          //sort each doctor's organizations by availability
+          doctors = doctors.map(
+            (e) {
+              e.organizations?.sort(orderByAvailability);
+              return e;
+            }
+          ).toList();
+
+          //sort doctors by first availability
+          doctors.sort(orderByOrganizationAvailability);
+          emit(DoctorsLoaded(doctors: doctors));
           emit(FilterSucces());
         }
       } else if (event is GetDoctorFilterInDoctorList) {
@@ -125,7 +120,8 @@ class DoctorsAvailableBloc
             0,
             event.specializations,
             event.virtualAppointment,
-            event.inPersonAppointment)).attempt().run().then((value) {
+            event.inPersonAppointment,
+            event.organizations)).attempt().run().then((value) {
           _post = value;
         });
         var response;
@@ -140,5 +136,33 @@ class DoctorsAvailableBloc
         }
       }
     });
+  }
+
+  int orderByAvailability(OrganizationWithAvailability a, OrganizationWithAvailability b){
+    if (b.nextAvailability == null && a.nextAvailability == null) {
+      return 0;
+    }
+    if (a.nextAvailability == null) {
+      return 1;
+    }
+    if (b.nextAvailability == null) {
+      return -1;
+    }
+    return DateTime.parse(a.nextAvailability!.availability!)
+        .compareTo(DateTime.parse(b.nextAvailability!.availability!));
+  }
+
+  int orderByOrganizationAvailability(Doctor a, Doctor b){
+    if (b.organizations?.first.nextAvailability == null && a.organizations?.first.nextAvailability == null) {
+      return 0;
+    }
+    if (a.organizations?.first.nextAvailability == null) {
+      return 1;
+    }
+    if (b.organizations?.first.nextAvailability == null) {
+      return -1;
+    }
+    return DateTime.parse(a.organizations!.first.nextAvailability!.availability!)
+        .compareTo(DateTime.parse(b.organizations!.first.nextAvailability!.availability!));
   }
 }
