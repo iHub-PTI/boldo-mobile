17f3432dfe2936558f641f6265a1589ad027ab89
Ever Garay <everfgmolinas@hotmail.com>
2023-01-27 12:26:08 -0300

    feat: generic widget to show appointment type

---
+++ lib/widgets/appointment_type_icon.dart
@@ -0,0 +1,42 @@
+import 'package:boldo/constants.dart';
+import 'package:boldo/utils/helpers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+Widget showAppointmentTypeIcon(AppointmentType? appointmentType) {
+
+  Widget icon;
+
+  switch (appointmentType){
+    case AppointmentType.InPerson:
+      icon = Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Container(
+          child: SvgPicture.asset(
+            'assets/icon/person.svg',
+            color: Constants.primaryColor500,
+          ),
+        ),
+      );
+      break;
+    case AppointmentType.Virtual:
+      icon = Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Container(
+            child: SvgPicture.asset(
+              'assets/icon/video.svg',
+              color: Constants.secondaryColor500,
+            ),
+          )
+      );
+      break;
+    case null:
+      icon = const Icon(Icons.error);
+      break;
+  }
+
+  return Container(
+    padding: const EdgeInsets.symmetric(horizontal: 6),
+    child: icon
+  );
+}
