91629d4117997081d2e6ceac16be8fedd7f3ca99
Ever Garay <everfgmolinas@hotmail.com>
2023-03-22 14:03:29 -0300

    fix: add padding to header

--- lib/widgets/header_page.dart
+++ lib/widgets/header_page.dart
@@ -6,43 +6,46 @@ import 'package:flutter/cupertino.dart';
 Widget header(String? text, String? patientText,
     {double? height, double? width, bool border = true, Color? borderColor}
     ) {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: [
-      prefs.getBool(isFamily) ?? false ?
-      Flexible(child: Row(
-        children: [
-          Text(
-            '$patientText de ',
-            style: boldoScreenTitleTextStyle.copyWith(
-                color: ConstantsV2.inactiveText),
-          ),
-          Expanded(
-            child: Container(
-              child: Text(
-                '${patient.givenName} ${patient.familyName}',
-                style: boldoScreenTitleTextStyle.copyWith(
-                    color: ConstantsV2.green
-                ),
-                overflow: TextOverflow.ellipsis,
-              ),
+  return Container(
+    padding: const EdgeInsets.all(16.0),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        prefs.getBool(isFamily) ?? false ?
+        Flexible(child: Row(
+          children: [
+            Text(
+              '$patientText de ',
+              style: boldoScreenTitleTextStyle.copyWith(
+                  color: ConstantsV2.inactiveText),
             ),
-          )
-        ],
-      )):
-      Text(
-        text?? "Cacecera",
-        style: boldoScreenTitleTextStyle.copyWith(color: ConstantsV2.activeText),
-      ),
-      ImageViewTypeForm(
-        height: height?? 60,
-        width: width?? 60,
-        border: border,
-        url: patient.photoUrl,
-        gender: patient.gender,
-        borderColor: borderColor,
-        elevation: 0,
-      ),
-    ],
+            Expanded(
+              child: Container(
+                child: Text(
+                  '${patient.givenName} ${patient.familyName}',
+                  style: boldoScreenTitleTextStyle.copyWith(
+                      color: ConstantsV2.green
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            )
+          ],
+        )):
+        Text(
+          text?? "Cacecera",
+          style: boldoScreenTitleTextStyle.copyWith(color: ConstantsV2.activeText),
+        ),
+        ImageViewTypeForm(
+          height: height?? 60,
+          width: width?? 60,
+          border: border,
+          url: patient.photoUrl,
+          gender: patient.gender,
+          borderColor: borderColor,
+          elevation: 0,
+        ),
+      ],
+    ),
   );
 }
