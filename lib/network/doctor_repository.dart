0dc3d8abf83096f37821b14ce29ccd7d19dd8244
Ever Garay <everfgmolinas@hotmail.com>
2023-05-16 09:07:30 -0400

    feat: get recent Doctors

--- lib/network/doctor_repository.dart
+++ lib/network/doctor_repository.dart
@@ -117,4 +117,83 @@ class DoctorRepository {
     }
   }
 
+  Future<List<Doctor>> getRecentDoctors(
+      int offset,
+      List<Specializations> specializations,
+      bool virtualAppointment,
+      bool inPersonAppointment,
+      List<Organization> organizations,
+      List<String> names,
+      ) async {
+    try {
+      // list of IDs
+      List<String> listOfSpecializations =
+      specializations.map((e) => e.id!).toList();
+
+      // list of names split for spaces
+      String listOfNames = names.join(" ");
+
+      // list of organizations IDs
+      String listOfOrganizations =
+      organizations.map((e) => e.id!).toList().join(",");
+
+      String? appointmentType;
+      // here set the type of appointment
+      if (virtualAppointment && inPersonAppointment) {
+        appointmentType = "AV";
+      } else if (virtualAppointment) {
+        appointmentType = "V";
+      } else if (inPersonAppointment) {
+        appointmentType = "A";
+      }
+      dynamic queryParams = {
+        "appointmentType": appointmentType,
+        "specialtyIds": listOfSpecializations,
+        "offset": offset,
+        "count": offset + 20,
+        "organizations": listOfOrganizations == ""? null : listOfOrganizations,
+        "names": listOfNames.split(" "),
+      };
+
+      // remove null values to solve null compare in server
+      queryParams.removeWhere((key, value) => value == null);
+
+      Response response;
+      if (prefs.getBool('isFamily') ?? false) {
+        response = await dio.get('/profile/caretaker/dependent/${patient.id}/recent/doctors',
+            queryParameters: queryParams
+        );
+      } else {
+        // the query is made
+        response = await dio.get('/profile/patient/recent/doctors',
+            queryParameters: queryParams
+        );
+      }
+      if (response.statusCode == 200) {
+        List<Doctor> doctors = List<Doctor>.from(response.data.map((i) => Doctor.fromJson(i)));
+        return doctors;
+      }
+      throw Failure('No se pudo obtener la lista de médicos');
+    } on DioError catch(ex) {
+      await Sentry.captureMessage(
+        ex.toString(),
+        params: [
+          {
+            "path": ex.requestOptions.path,
+            "data": ex.requestOptions.data,
+            "patient": prefs.getString("userId"),
+            "responseError": ex.response,
+          }
+        ],
+      );
+      throw Failure('No se pudo obtener la lista de médicos');
+    } catch (exception, stackTrace) {
+      await Sentry.captureException(
+        exception,
+        stackTrace: stackTrace,
+      );
+      throw Failure(genericError);
+    }
+  }
+
 }
