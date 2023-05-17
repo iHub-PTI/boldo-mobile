d51d7cf90f5395070cfc9a5d1f128caaf854db88
Ever Garay <everfgmolinas@hotmail.com>
2023-05-17 08:09:01 -0400

    feat: bloc to put favorite status of doctors

---
+++ lib/blocs/favorite_action_bloc/favorite_action_bloc.dart
@@ -0,0 +1,39 @@
+import 'package:boldo/models/Doctor.dart';
+import 'package:boldo/network/doctor_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rxdart/rxdart.dart';
+
+
+part 'favorite_action_event.dart';
+part 'favorite_action_state.dart';
+
+class FavoriteActionBloc extends Bloc<FavoriteActionEvent, FavoriteActionState> {
+  final DoctorRepository _doctorRepository = DoctorRepository();
+  FavoriteActionBloc() : super(DoctorAvailabilityInitial()) {
+    on<FavoriteActionEvent>((event, emit) async {
+      if (event is PutFavoriteStatus) {
+        emit(LoadingFavoriteAction());
+        var _post;
+        await Task(() =>
+            _doctorRepository
+                .putFavoriteStatus(
+                event.doctor,
+                event.favoriteStatus,
+            )
+        ).attempt().run().then((value) {
+          _post = value;
+        });
+        var response;
+        if (_post.isLeft()) {
+          _post.leftMap((l) => response = l.message);
+          emit(FailedFavoriteAction(response: response));
+        } else {
+
+          emit(SuccessFavoriteAction());
+        }
+      }
+    });
+  }
+}
