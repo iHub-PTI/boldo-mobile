92b0755d8cd97c7f34c400343f2a6c0f6ff2e78a
Ever Garay <everfgmolinas@hotmail.com>
2023-05-31 12:39:23 -0400

    style: update privacy policy and terms of services

--- lib/screens/privacy_policy/privacy_policy.dart
+++ lib/screens/privacy_policy/privacy_policy.dart
@@ -99,7 +99,7 @@ class _PrivacyPolicyState extends State<PrivacyPolicy> {
             child: RichText(
               text: TextSpan(
                 text:
-                    'Usted acepta esta Política de Privacidad al registrarse, acceder o utilizar los servicios, contenido, funcionalidades, tecnología o funciones disponibles de Mi Pasaporte del Ecosistema de Boldo. \n\n',
+                    'Usted acepta esta Política de Privacidad al registrarse, acceder o utilizar los servicios, contenido, funcionalidades, tecnología o funciones disponibles de Boldo. \n\n',
                 style: const TextStyle(
                   color: Color.fromRGBO(54, 65, 82, 1),
                   fontFamily: 'PT Serif',
@@ -116,21 +116,33 @@ class _PrivacyPolicyState extends State<PrivacyPolicy> {
                   //     text: 'Boldo', style: TextStyle(fontWeight: FontWeight.bold)),
                   const TextSpan(
                       text:
-                          'Recopilamos la siguiente información personal con el fin de proporcionarle el uso de los servicios de Mi Pasaporte y poder personalizar y mejorar su experiencia.   \n\n',
+                          'Recopilamos la siguiente información personal con el fin de proporcionarle el uso de los servicios de Boldo y poder personalizar y mejorar su experiencia.   \n\n',
                       style: TextStyle(fontWeight: FontWeight.normal)),
                   // TextSpan(
                   //     text: 'Receta médica electronica (RME)',
                   //     style: TextStyle(fontWeight: FontWeight.bold)),
                   const TextSpan(
                       text:
-                          'Información que recopilamos automáticamente: información que nos envía su teléfono móvil u otro dispositivo de acceso: dirección IP del dispositivo, identificación del dispositivo o identificador único, tipo de dispositivo, identificación del navegador e información de geolocalización.   \n\n',
+                          'Información que recopilamos automáticamente: información que nos envía su teléfono móvil u otro dispositivo de acceso: dirección de IP del dispositivo, identificación del dispositivo o identificador único, tipo de dispositivo, identificación del navegador e información de geolocalización.   \n\n',
                       style: TextStyle(fontWeight: FontWeight.normal)),
                   // TextSpan(
                   //     text: 'Consulta médica remota (CMR)',
                   //     style: TextStyle(fontWeight: FontWeight.bold)),
                   const TextSpan(
                       text:
-                          'Información que usted nos proporciona: cualquier información que nos proporcione al usar Mi Pasaporte, incluso cuando incluye información en un formulario web, incorpora o actualiza la información de su cuenta, participa en discusiones, charlas o resoluciones de controversias en la comunidad, o cuando se comunica con nosotros para tratar acerca de los servicios Mi Pasaporte: información de contacto, como su nombre y apellido, sexo, fecha de nacimiento, número de documento de identidad, tipo de documento, dirección, número de teléfono, correo electrónico y otra información similar; información médica, como diagnósticos, recetas, medicamentos que consume, le son recetados y/o dispensados, indicaciones médicas, etc.    \n\n',
+                          'Información que usted nos proporciona: cualquier información que nos proporcione al usar Boldo, incluso cuando incluye información en un formulario web, incorpora o actualiza la información de su cuenta, participa en discusiones, charlas o resoluciones de controversias en la comunidad, o cuando se comunica con nosotros para tratar acerca de los servicios Boldo: información de contacto, como su nombre y apellido, sexo, fecha de nacimiento, número de documento de identidad, tipo de documento, dirección, número de teléfono, correo electrónico y otra información similar; información médica, como diagnósticos, recetas, medicamentos que consume, le son recetados y/o dispensados, indicaciones médicas, etc.    \n\n',
+                      style: TextStyle(fontWeight: FontWeight.normal)),
+                  const TextSpan(
+                      text:
+                      'También podemos recopilar información de usted o acerca de usted de otras fuentes, como por ejemplo, a través de comunicaciones con nosotros, lo que incluye a nuestro equipo de soporte técnico, sus resultados cuando responde una encuesta y sus interacciones con los miembros del equipo de Boldo u otras empresas (sujeto a sus políticas de privacidad y a las leyes vigentes). Además, para fines de calidad y capacitación o por su propia protección, Boldo puede controlar o grabar las conversaciones telefónicas que tengamos con usted o con cualquier persona que actúe en su nombre. Al comunicarse con el soporte técnico de Boldo, usted reconoce que la comunicación podría ser escuchada, controlada o grabada sin notificación o advertencia.    \n\n',
+                      style: TextStyle(fontWeight: FontWeight.normal)),
+                  const TextSpan(
+                      text:
+                      'Puede optar por proporcionarnos acceso a cierta información personal almacenada por terceros, tales como sitios de redes sociales (por ejemplo, Facebook y Twitter). La información que podemos recibir varía según el sitio y es controlada por dicho sitio. Al asociar una cuenta administrada por un tercero con su cuenta de Boldo y al autorizar a Boldo a tener acceso a esta información, usted acepta que Boldo pueda recopilar, almacenar y utilizar esta información de acuerdo con esta Política de Privacidad.    \n\n',
+                      style: TextStyle(fontWeight: FontWeight.normal)),
+                  const TextSpan(
+                      text:
+                      'Autenticación y detección de fraudes: A fin de poder protegerlo contra fraudes y el mal uso de su información personal, podemos recopilar información acerca de usted y de sus interacciones con el servicio de Boldo.    \n\n',
                       style: TextStyle(fontWeight: FontWeight.normal)),
                   TextSpan(
                       text:
@@ -193,7 +205,7 @@ class _PrivacyPolicyState extends State<PrivacyPolicy> {
                       style: TextStyle(fontWeight: FontWeight.normal)),
                   const TextSpan(
                       text:
-                          'Podemos contactarlo por vía electrónica o por correo postal para notificarlo de información relacionada con su cuenta, solucionar problemas con su cuenta, resolver una controversia, agrupar sus opiniones a través de encuestas o cuestionarios o de otro modo, según sea necesario para brindar servicio técnico a su cuenta. Además, podemos contactarlo para ofrecerle cupones, descuentos y promociones, y para informarle acerca de Mi Pasaporte.   \n\n',
+                          'Podemos contactarlo por vía electrónica o por correo postal para notificarlo de información relacionada con su cuenta, solucionar problemas con su cuenta, resolver una controversia, agrupar sus opiniones a través de encuestas o cuestionarios o de otro modo, según sea necesario para brindar servicio técnico a su cuenta. Además, podemos contactarlo para ofrecerle cupones, descuentos y promociones, y para informarle acerca de Boldo.   \n\n',
                       style: TextStyle(fontWeight: FontWeight.normal)),
                   const TextSpan(
                       text:
@@ -208,15 +220,19 @@ class _PrivacyPolicyState extends State<PrivacyPolicy> {
                       style: TextStyle(fontWeight: FontWeight.normal)),
                   const TextSpan(
                       text:
-                          'En caso de que se detecte algún servicio fraudulento, Mi Pasaporte notificará a los usuarios afectados con todos los datos del responsable.     \n\n',
+                          'En caso de que se detecte algún servicio fraudulento, Boldo notificará a los usuarios afectados con todos los datos del responsable.     \n\n',
                       style: TextStyle(fontWeight: FontWeight.normal)),
                   const TextSpan(
                       text:
-                          'Mi Pasaporte generará información médica estadística y podrá compartir, a su solo criterio, con autoridades de salud nacionales, departamentales y municipales. No se compartirá, salvo medidas legales especiales, información personal individualizada.      \n\n',
+                          'Boldo generará información médica estadística y podrá compartir, a su solo criterio, con autoridades de salud nacionales, departamentales y municipales. No se compartirá, salvo medidas legales especiales, información personal individualizada.      \n\n',
                       style: TextStyle(fontWeight: FontWeight.normal)),
                   const TextSpan(
                       text:
-                          'Mi Pasaporte generará información médica estadística y podrá compartir, a su solo criterio, con autoridades y organismos de salud internacionales y transnacionales. No se compartirá, salvo medidas legales especiales, información personal individualizada.    \n\n',
+                          'Boldo generará información médica estadística y podrá compartir, a su solo criterio, con autoridades y organismos de salud internacionales y transnacionales. No se compartirá, salvo medidas legales especiales, información personal individualizada.    \n\n',
+                      style: TextStyle(fontWeight: FontWeight.normal)),
+                  const TextSpan(
+                      text:
+                      'Boldo generará información estadística de medicamentos recetados y/o dispensados y podrá comercializar y/o compartir, a su solo criterio, con empresas farmacéuticas y laboratorios. No se compartirá, salvo medidas legales especiales, información personal individualizada.    \n\n',
                       style: TextStyle(fontWeight: FontWeight.normal)),
                 ],
               ),
