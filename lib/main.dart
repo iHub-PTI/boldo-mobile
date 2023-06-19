e13614433510d4a0c5d81b44875f10e6504fc18a
Ever Garay <everfgmolinas@hotmail.com>
2023-06-19 14:50:44 -0400

    feat: init app with sentry on releaseMode to catch performance

--- lib/main.dart
+++ lib/main.dart
@@ -158,11 +158,38 @@ Future<void> main() async {
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
-  ]).then((value) => runApp(MyApp(
-    session: session ?? '',
-    hasUpdate: hasUpdate,
-    hasRequiredUpdate: hasRequiredUpdate,
-  )));
+  ]);
+
+  if (kReleaseMode) {
+    String? sentryDSN = environment.SENTRY_DSN;
+    await SentryFlutter.init(
+      (options) {
+        options.environment = environment.SENTRY_ENV;
+        options.dsn = sentryDSN;
+        options.tracesSampleRate = appConfig.TRACE_RATE_ERROR;
+        options.captureFailedRequests = true;
+      },
+      appRunner: ()=>{
+
+        dio.addSentry(),
+        dioDownloader.addSentry(),
+        dioPassport.addSentry(),
+
+        runApp(MyApp(
+          session: session ?? '',
+          hasUpdate: hasUpdate,
+          hasRequiredUpdate: hasRequiredUpdate,
+        ))
+      },
+    );
+  }else{
+    runApp(MyApp(
+      session: session ?? '',
+      hasUpdate: hasUpdate,
+      hasRequiredUpdate: hasRequiredUpdate,
+    ));
+  }
+
 }
 
 class MyApp extends StatefulWidget {
