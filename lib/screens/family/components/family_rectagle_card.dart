cb2117087b4cad954474059db6244358b403760e
Ever Garay <everfgmolinas@hotmail.com>
2023-08-03 16:17:25 -0400

    refactor: Add Qr step in Principal card

--- lib/screens/family/components/family_rectagle_card.dart
+++ lib/screens/family/components/family_rectagle_card.dart
@@ -1,7 +1,9 @@
 import 'package:boldo/blocs/family_bloc/dependent_family_bloc.dart';
 import 'package:boldo/models/Patient.dart';
+import 'package:boldo/screens/family/tabs/QR_generator.dart';
 import 'package:boldo/screens/family/tabs/my_managers_tab.dart';
 import 'package:boldo/screens/profile/components/profile_image.dart';
+import 'package:boldo/utils/helpers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -32,91 +34,153 @@ class _FamilyRectangleCardState extends State<FamilyRectangleCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Card(
-        child: InkWell(
-          onTap: widget.isDependent ? (){} : (){
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => MyManagersTab()
-              ),
-            );
-          },
-          child: Container(
-            width: MediaQuery.of(context).size.width-16,
-            child: Row(
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(left: 16, top: 8, bottom: 8, right: 8),
-                  child: widget.isDependent
-                      ? ImageViewTypeForm(height: 60, width: 60, border: false, url: widget.patient?.photoUrl, gender: widget.patient?.photoUrl,)
-                      : ImageViewTypeForm(height: 60, width: 60, border: false, url: prefs.getString('profile_url'), gender: prefs.getString('gender')),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      decoration: BoxDecoration(
+        color: ConstantsV2.lightest,
+        boxShadow: [
+          shadowRegular,
+        ]
+      ),
+      child: Column(
+        children: [
+          InkWell(
+            onTap: widget.isDependent ? (){} : (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => MyManagersTab()
                 ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.only(right: 8),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Flexible(
-                              child: widget.isDependent
-                                  ? Text(
-                                "${widget.patient!.givenName} ${widget.patient!.familyName}",
-                                style: boldoSubTextMediumStyle.copyWith(
-                                    color: ConstantsV2.activeText
+              );
+            },
+            child: Container(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    child: widget.isDependent
+                        ? ImageViewTypeForm(height: 60, width: 60, border: false, url: widget.patient?.photoUrl, gender: widget.patient?.photoUrl,)
+                        : ImageViewTypeForm(height: 60, width: 60, border: false, url: prefs.getString('profile_url'), gender: prefs.getString('gender')),
+                  ),
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: 61,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Flexible(
+                                      child: widget.isDependent
+                                          ? Text(
+                                        "${widget.patient!.givenName} ${widget.patient!.familyName}",
+                                        style: boldoSubTextMediumStyle.copyWith(
+                                            color: ConstantsV2.activeText
+                                        ),
+                                      )
+                                          :Text(
+                                        "${prefs.getString('name') ?? ''} ${prefs.getString('lastName') ?? ''}",
+                                        style: boldoSubTextMediumStyle.copyWith(
+                                            color: ConstantsV2.activeText
+                                        ),
+                                      ),
+                                    ),
+                                    widget.isDependent && !(prefs.getBool(isFamily)?? false) ? UnlinkFamilyWidget(
+                                      onTapCallback: (result) async {
+                                        if (result == 'Desvincular') {
+                                          String? action = await unlinkFamilyDialog(context);
+                                          if(action == 'cancel') {
+                                            BlocProvider.of<FamilyBloc>(context).add(
+                                                UnlinkDependent(
+                                                    id: widget.patient!.id!));
+                                          }
+                                        }
+                                      },
+                                    ): const Icon(
+                                      Icons.chevron_right_rounded,
+                                      size: 24,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Text(
+                                ! widget.isDependent ? "mi perfil" : widget.patient!.relationshipDisplaySpan?.capitalize()??'',
+                                style: boldoCorpMediumTextStyle.copyWith(
+                                  color: ConstantsV2.green,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        if(widget.isDependent)
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text("agregado el ${formatDateToString(
+                                    widget.patient?.startDependenceDate?? DateTime.now().toString()
+                                )}",
+                                style: boldoCorpSmallTextStyle.copyWith(
+                                  color: ConstantsV2.inactiveText,
                                 ),
                               )
-                                  :Text(
-                                "${prefs.getString('name') ?? ''} ${prefs.getString('lastName') ?? ''}",
-                                style: boldoSubTextMediumStyle.copyWith(
-                                    color: ConstantsV2.activeText
-                                ),
-                              ),
-                            ),
-                            widget.isDependent && !(prefs.getBool(isFamily)?? false) ? UnlinkFamilyWidget(
-                              onTapCallback: (result) async {
-                                if (result == 'Desvincular') {
-                                  String? action = await unlinkFamilyDialog(context);
-                                  if(action == 'cancel') {
-                                    BlocProvider.of<FamilyBloc>(context).add(
-                                        UnlinkDependent(
-                                            id: widget.patient!.id!));
-                                  }
-                                }
-                              },
-                            ): Container(),
-                          ],
-                        ),
-                      ),
-                      Text(
-                        ! widget.isDependent ? "mi perfil" : widget.patient!.relationshipDisplaySpan??'',
-                        style: boldoCorpMediumTextStyle.copyWith(
-                          color: ConstantsV2.green,
-                        ),
-                      ),
-                      if(widget.isDependent)
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text("agregado el ${widget.patient!.startDependenceDate!}",
-                              style: boldoCorpSmallTextStyle.copyWith(
-                                color: ConstantsV2.inactiveText,
-                              ),
-                            )
-                          ],
-                        ),
-                    ],
+                            ],
+                          ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-      ),
+          if(!widget.isDependent)
+            const SizedBox(
+              height: 8,
+            ),
+          if(!widget.isDependent)
+            InkWell(
+              onTap: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const QRGenerator()
+                  ),
+                );
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                child: Row(
+                  children: [
+                    SvgPicture.asset(
+                      'assets/icon/qrcode.svg',
+                      color: ConstantsV2.secondaryRegular,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Flexible(
+                      child: Text(
+                        'Vincular un gestor para mi perfil',
+                        style: boldoCorpMediumTextStyle.copyWith(
+                          color: ConstantsV2.secondaryRegular,
+                          fontWeight: FontWeight.w400,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+        ],
+      )
     );
   }
 
