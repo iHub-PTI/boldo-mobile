bf6a0186eb1e5c3d8cadf5db0444819a40b207ef
Ever Garay <everfgmolinas@hotmail.com>
2023-08-03 16:15:46 -0400

    fix: remove unnecessary code

--- lib/screens/family/family_tab.dart
+++ lib/screens/family/family_tab.dart
@@ -140,7 +140,6 @@ class _FamilyScreenState extends State<FamilyScreen> {
                         ],
                       ),
                     ),
-                    ),
                   ],
                 ),
               ),
