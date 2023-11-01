19e3722236b687871c057c6775ab40c4c7e70d5d
Ever Garay <everfgmolinas@hotmail.com>
2023-11-01 17:43:03 -0300

    feat: create function to download file

--- lib/network/files_repository.dart
+++ lib/network/files_repository.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:boldo/models/upload_url_model.dart';
 import 'package:boldo/network/repository_helper.dart';
+import 'package:boldo/utils/errors.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:path/path.dart' as path;
@@ -42,4 +44,40 @@ class FilesRepository {
     }
     throw Failure('Unknown StatusCode ${response.statusCode}',);
   }
+
+  /// If [localDio] isn't passed, by default it will download with http
+  /// connection
+  ///
+  /// [queryParams] is used with [localDio]
+  ///
+  /// return the file as bytes
+  static Future<Uint8List> getFile({
+      Dio? localDio,
+      Map<String, dynamic>? queryParams,
+      final url,
+  }) async {
+    try {
+
+      var data;
+
+      if(localDio!= null){
+        data = await localDio.get(url, queryParameters: queryParams);
+      }else{
+        data = await http.get(Uri.parse(url));
+      }
+
+      Uint8List bytes = data.bodyBytes;
+      return bytes;
+    } catch (exception, stackTrace) {
+      captureError(
+        exception: exception,
+        stackTrace: stackTrace,
+        data: {
+          'url': url,
+        },
+      );
+      throw Failure("Error al descargar el archivo");
+    }
+  }
+
 }
