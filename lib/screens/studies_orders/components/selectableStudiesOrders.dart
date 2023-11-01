8a4b085eb16b2eb49a0d89f690bb72b23c27c487
Ever Garay <everfgmolinas@hotmail.com>
2023-11-01 14:34:21 -0300

    style: improve animation to scale transition with fade effect

--- lib/screens/studies_orders/components/selectableStudiesOrders.dart
+++ lib/screens/studies_orders/components/selectableStudiesOrders.dart
@@ -105,110 +105,124 @@ class SelectableServiceRequestState extends State<SelectableServiceRequest> with
   }
 
   Widget tabDownload(){
-    return AnimatedSize(
+    return AnimatedCrossFade(
+      crossFadeState: _controller.isAnimating || _controller.isCompleted? CrossFadeState.showSecond: CrossFadeState.showFirst,
       duration: durationEffect,
-      child: Container(
-        height: _controller.isCompleted? null : 0,
-        decoration: ShapeDecoration(
-          color: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(7.53),
-          ),
-        ),
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      const SizedBox(width: 5,),
-                      Material(
-                        color: Colors.transparent,
-                        child: InkWell(
-                          onTap: (){
-                            selectAll =false;
-                            listSelectableElements.updateAll((key, value) => value = selectAll);
-                            checkHeader();
-                          },
-                          child: const Icon(
-                            Icons.arrow_back_rounded,
-                            color: ConstantsV2.primaryRegular,
-                            size: 22,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 15,),
-                      AnimatedSwitcher(
-                        duration: durationEffect,
-                        child: Text(
-                          key: Key("${listSelectableElements.values.where((element) => element==true).length}"),
-                          "${listSelectableElements.values.where((element) => element==true).length}",
-                          style: GoogleFonts.montserrat(
-                            fontWeight: FontWeight.w500,
-                            color: ConstantsV2.blueDark,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Material(
-                    color: Colors.transparent,
-                    child: InkWell(
-                      onTap: (){
-                      },
-                      child: Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            const Icon(
-                              Icons.save_alt_rounded,
-                              color: ConstantsV2.grayDark,
-                            ),
-                            const SizedBox(width: 8,),
-                            Text(
-                              "Descargar",
-                              style: GoogleFonts.inter(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w500,
-                                letterSpacing: 0.15,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+      firstChild: Container(),
+      secondChild: AnimatedSwitcher(
+        switchInCurve: Curves.linear,
+        switchOutCurve: Curves.linear,
+        duration: durationEffect,
+        transitionBuilder: (Widget child, Animation<double> animation) {
+          return ScaleTransition(
+            scale: animation,
+            child: child,
+          );
+        },
+        child: Container(
+          key: _controller.isAnimating || _controller.isCompleted? Key('header') : null,
+          decoration: ShapeDecoration(
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(7.53),
             ),
-            Container(
-                padding: const EdgeInsets.only(left: 8, right: 8),
+          ),
+          child: _controller.isAnimating || _controller.isCompleted? Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(8),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Checkbox(
-                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      value: selectAll,
-                      activeColor: ConstantsV2.orange,
-                      checkColor: Colors.white,
-                      onChanged: (value) {
-                        selectAll = !selectAll;
-                        listSelectableElements.updateAll((key, value) => value = selectAll);
-                        checkHeader();
-                      },
+                    Row(
+                      children: [
+                        const SizedBox(width: 5,),
+                        Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            onTap: (){
+                              selectAll =false;
+                              listSelectableElements.updateAll((key, value) => value = selectAll);
+                              checkHeader();
+                            },
+                            child: const Icon(
+                              Icons.arrow_back_rounded,
+                              color: ConstantsV2.primaryRegular,
+                              size: 22,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 15,),
+                        AnimatedSwitcher(
+                          duration: durationEffect,
+                          child: Text(
+                            //use key to detect change on child o animatedSwitcher
+                            key: Key("${listSelectableElements.values.where((element) => element==true).length}"),
+                            "${listSelectableElements.values.where((element) => element==true).length}",
+                            style: GoogleFonts.montserrat(
+                              fontWeight: FontWeight.w500,
+                              color: ConstantsV2.blueDark,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                    const Flexible(
-                      child: Text(
-                        chooseAll,
-                        style: TextStyle(color: ConstantsV2.activeText),
+                    Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        onTap: (){
+                        },
+                        child: Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              const Icon(
+                                Icons.save_alt_rounded,
+                                color: ConstantsV2.grayDark,
+                              ),
+                              const SizedBox(width: 8,),
+                              Text(
+                                "Descargar",
+                                style: GoogleFonts.inter(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                  letterSpacing: 0.15,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
                     ),
                   ],
-                )
-            ),
-          ],
+                ),
+              ),
+              Container(
+                  padding: const EdgeInsets.only(left: 8, right: 8),
+                  child: Row(
+                    children: [
+                      Checkbox(
+                        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        value: selectAll,
+                        activeColor: ConstantsV2.orange,
+                        checkColor: Colors.white,
+                        onChanged: (value) {
+                          selectAll = !selectAll;
+                          listSelectableElements.updateAll((key, value) => value = selectAll);
+                          checkHeader();
+                        },
+                      ),
+                      const Flexible(
+                        child: Text(
+                          chooseAll,
+                          style: TextStyle(color: ConstantsV2.activeText),
+                        ),
+                      ),
+                    ],
+                  )
+              ),
+            ],
+          ) : null,
         ),
       ),
     );
