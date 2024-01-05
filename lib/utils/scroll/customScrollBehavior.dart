4c26a083a0b8fd2b34036168a12987fccf2296cf
Ever Garay <everfgmolinas@hotmail.com>
2024-01-05 15:13:27 -0300

    feat: create CustomBehavior to remove glow effect on android scroll

---
+++ lib/utils/scroll/customScrollBehavior.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomBehavior extends ScrollBehavior {
+  @override
+  Widget buildOverscrollIndicator(
+      BuildContext context,
+      Widget child,
+      ScrollableDetails details,
+      ) {
+    return child;
+  }
+}
