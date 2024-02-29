5cda3d1d75ab723ce58ee633174757718d15c044
Ever Garay <everfgmolinas@hotmail.com>
2024-02-29 15:03:06 -0300

    refactor: move components to rename folder to correct name

--- lib/screens/organizations/request_subscription/RequestRequirementPostulation.dart
+++ lib/screens/organizations/request_subscription/RequestRequirementPostulation.dart
@@ -1,7 +1,7 @@
 import 'package:boldo/blocs/organizationRequirementsValidate_bloc/organizationRequirementsValidate_bloc.dart';
 import 'package:boldo/constants.dart';
 import 'package:boldo/models/Organization.dart';
-import 'package:boldo/screens/organizations/request_suscription/components/confirm_veracity.dart';
+import 'package:boldo/screens/organizations/request_subscription/components/confirm_veracity.dart';
 import 'package:boldo/widgets/back_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
