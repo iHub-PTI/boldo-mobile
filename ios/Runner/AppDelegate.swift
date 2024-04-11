f8e70f1e157a26000df127efbf5a1f3b6640a636
Ever Garay <everfgmolinas@hotmail.com>
2024-04-11 14:58:27 -0400

    build: get dart_defines values for pre compile actions

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,13 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let dartDefinesString = Bundle.main.infoDictionary!["DART_DEFINES"] as! String
+    var dartDefinesDictionary = [String:String]()
+    for definedValue in dartDefinesString.components(separatedBy: ",") {
+        let decoded = String(data: Data(base64Encoded: definedValue)!, encoding: .utf8)!
+        let values = decoded.components(separatedBy: "=")
+        dartDefinesDictionary[values[0]] = values[1]
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
