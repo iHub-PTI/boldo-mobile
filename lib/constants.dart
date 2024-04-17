7d9efa1c5fe57e2ccbe546bb6342fb87d8ee75c6
Gabriel <maldoangel@hotmail.com>
2024-04-17 11:36:57 -0400

    fix: botones con letras/iconos lila

--- lib/constants.dart
+++ lib/constants.dart
@@ -175,12 +175,11 @@ TextStyle bigButton = GoogleFonts.montserrat().copyWith(
 );
 
 const boldoInfoTextStyle = TextStyle(
-  color: ConstantsV2.activeText,
-  fontFamily: 'Montserrat',
-  fontStyle: FontStyle.normal,
-  fontWeight: FontWeight.w300,
-  fontSize: 14
-);
+    color: ConstantsV2.activeText,
+    fontFamily: 'Montserrat',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w300,
+    fontSize: 14);
 
 const boldoCardHeadingTextStyle = TextStyle(
   fontStyle: FontStyle.normal,
@@ -363,32 +362,28 @@ BoxShadow shadowHourAvailable = BoxShadow(
   spreadRadius: 0,
 );
 
-const BoxDecoration buttonFXSecondaryStyle = BoxDecoration(
-  boxShadow: [
-    BoxShadow(
-      offset: Offset(0, 2),
-      color: Color(0xffFEAD88),
-      spreadRadius: -4,
-      blurRadius: 4,
-    ),
-    BoxShadow(
-      offset: Offset(0, 2),
-      color: Color.fromRGBO(0, 0, 0, 0.1),
-      blurRadius: 4,
-    ),
-    BoxShadow(
-      offset: Offset(0, 0),
-      color: Color.fromRGBO(253, 165, 125, 0.5),
-      blurRadius: 4,
-    )
-  ]
-);
+const BoxDecoration buttonFXSecondaryStyle = BoxDecoration(boxShadow: [
+  BoxShadow(
+    offset: Offset(0, 2),
+    color: Color(0xffFEAD88),
+    spreadRadius: -4,
+    blurRadius: 4,
+  ),
+  BoxShadow(
+    offset: Offset(0, 2),
+    color: Color.fromRGBO(0, 0, 0, 0.1),
+    blurRadius: 4,
+  ),
+  BoxShadow(
+    offset: Offset(0, 0),
+    color: Color.fromRGBO(253, 165, 125, 0.5),
+    blurRadius: 4,
+  )
+]);
 
 BoxDecoration cardDecoration = BoxDecoration(
   color: ConstantsV2.lightest,
-  boxShadow: [
-    shadowRegular
-  ],
+  boxShadow: [shadowRegular],
 );
 
 const BoxDecoration selectedCardDecoration = BoxDecoration(
@@ -410,144 +405,145 @@ const BoxDecoration selectedCardDecoration = BoxDecoration(
 );
 
 ThemeData boldoTheme = ThemeData(
-  useMaterial3: true,
-  fontFamily: 'Montserrat',
-  listTileTheme: const ListTileThemeData(
-    horizontalTitleGap: 0
-  ),
-  elevatedButtonTheme: ElevatedButtonThemeData(
-    style: ButtonStyle(
-      textStyle: MaterialStateProperty.all(
-        const TextStyle(fontWeight: FontWeight.w500, fontSize: 16,
-            color: ConstantsV2.BGNeutral
+    useMaterial3: true,
+    fontFamily: 'Montserrat',
+    listTileTheme: const ListTileThemeData(horizontalTitleGap: 0),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        textStyle: MaterialStateProperty.all(
+          const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 16,
+              color: ConstantsV2.BGNeutral),
+        ),
+        foregroundColor:
+            MaterialStateProperty.all<Color?>(ConstantsV2.primaryColor),
+        padding: MaterialStateProperty.all(
+          const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+        ),
+        backgroundColor: MaterialStateProperty.resolveWith<Color>((states) {
+          if (states.contains(MaterialState.disabled)) {
+            return ConstantsV2.gray; // Disabled color
+          }
+          return ConstantsV2.orange; // Regular color
+        }),
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100),
+          ),
         ),
       ),
-      padding: MaterialStateProperty.all(
-        const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        foregroundColor: ConstantsV2.secondaryRegular,
+        textStyle: boldoSubTextMediumStyle.copyWith(
+            color: ConstantsV2.secondaryRegular),
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
       ),
-      backgroundColor: MaterialStateProperty.resolveWith<Color>((states) {
-        if (states.contains(MaterialState.disabled)) {
-          return ConstantsV2.gray; // Disabled color
+    ),
+    toggleableActiveColor: ConstantsV2.orange,
+    checkboxTheme: CheckboxThemeData(
+      shape: RoundedRectangleBorder(
+        side: const BorderSide(color: ConstantsV2.orange, width: 5),
+        borderRadius: BorderRadius.circular(4),
+      ),
+      fillColor: MaterialStateProperty.resolveWith((states) {
+        if (states.contains(MaterialState.selected)) {
+          return ConstantsV2.orange;
         }
-        return ConstantsV2.orange; // Regular color
       }),
-      shape: MaterialStateProperty.all(
-        RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(100),
-        ),
-      ),
     ),
-  ),
-  textButtonTheme: TextButtonThemeData(
-    style: TextButton.styleFrom(
-      foregroundColor: ConstantsV2.secondaryRegular,
-      textStyle: boldoSubTextMediumStyle.copyWith(color: ConstantsV2.secondaryRegular),
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-    ),
-  ),
-  toggleableActiveColor: ConstantsV2.orange,
-  checkboxTheme: CheckboxThemeData(
-    shape: RoundedRectangleBorder(
-      side: const BorderSide(color: ConstantsV2.orange, width: 5),
-      borderRadius: BorderRadius.circular(4),
-    ),
-    fillColor: MaterialStateProperty.resolveWith((states) {
-      if(states.contains(MaterialState.selected)){
-        return ConstantsV2.orange;
-      }
-    }),
-  ),
-  unselectedWidgetColor: ConstantsV2.orange,
-  floatingActionButtonTheme: FloatingActionButtonThemeData(
-    extendedTextStyle:
-        const TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
-    extendedPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-    backgroundColor: ConstantsV2.orange,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(100),
-    ),
-  ),
-  outlinedButtonTheme: OutlinedButtonThemeData(
-    style: OutlinedButton.styleFrom(
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-      textStyle: const TextStyle(fontWeight: FontWeight.w400, fontSize: 16, color: ConstantsV2.orange),
-      foregroundColor: ConstantsV2.orange,
+    unselectedWidgetColor: ConstantsV2.orange,
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      extendedTextStyle:
+          const TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+      extendedPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+      backgroundColor: ConstantsV2.orange,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(100),
       ),
-      side: const BorderSide(
-        color: ConstantsV2.orange,
-        width: 1,
-      )
     ),
-  ),
-  textTheme: TextTheme(
-    titleMedium: boldoCorpSmallSTextStyle.copyWith(
-      color: ConstantsV2.activeText,
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+          textStyle: const TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+              color: ConstantsV2.orange),
+          foregroundColor: ConstantsV2.orange,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100),
+          ),
+          side: const BorderSide(
+            color: ConstantsV2.orange,
+            width: 1,
+          )),
     ),
-    bodyLarge: bodyLarge,
-  ),
-  inputDecorationTheme: InputDecorationTheme(
-    enabledBorder: UnderlineInputBorder(
-      borderRadius: BorderRadius.circular(6),
-      borderSide: const BorderSide(
-        color: ConstantsV2.enableBorded,
-        width: 1.0,
+    textTheme: TextTheme(
+      titleMedium: boldoCorpSmallSTextStyle.copyWith(
+        color: ConstantsV2.activeText,
       ),
+      bodyLarge: bodyLarge,
     ),
-    focusedBorder: UnderlineInputBorder(
-      borderRadius: BorderRadius.circular(6),
-      borderSide: const BorderSide(
-        color: ConstantsV2.focuseBorder,
-        width: 1.25,
-      ),
+    inputDecorationTheme: InputDecorationTheme(
+        enabledBorder: UnderlineInputBorder(
+          borderRadius: BorderRadius.circular(6),
+          borderSide: const BorderSide(
+            color: ConstantsV2.enableBorded,
+            width: 1.0,
+          ),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderRadius: BorderRadius.circular(6),
+          borderSide: const BorderSide(
+            color: ConstantsV2.focuseBorder,
+            width: 1.25,
+          ),
+        ),
+        focusedErrorBorder: UnderlineInputBorder(
+          borderRadius: BorderRadius.circular(6),
+          borderSide: const BorderSide(
+            color: ConstantsV2.buttonPrimaryColor100,
+          ),
+        ),
+        errorBorder: UnderlineInputBorder(
+          borderRadius: BorderRadius.circular(6),
+          borderSide: const BorderSide(
+            color: ConstantsV2.buttonPrimaryColor100,
+            width: 1.0,
+          ),
+        ),
+        errorStyle: bodySmallRegular.copyWith(color: ConstantsV2.systemFail)),
+    searchBarTheme: SearchBarThemeData(
+      elevation: MaterialStateProperty.all<double>(0),
+      backgroundColor: MaterialStateProperty.resolveWith((states) {
+        switch (states.firstOrNull) {
+          case MaterialState.selected:
+          case MaterialState.pressed:
+          case MaterialState.focused:
+            return ConstantsV2.gray.withOpacity(0.5);
+          default:
+            return ConstantsV2.BGNeutral;
+        }
+      }),
     ),
-    focusedErrorBorder: UnderlineInputBorder(
-      borderRadius: BorderRadius.circular(6),
-      borderSide: const BorderSide(
-        color: ConstantsV2.buttonPrimaryColor100,
-      ),
+    primaryColor: Colors.white,
+    scaffoldBackgroundColor: ConstantsV2.BGNeutral,
+    brightness: Brightness.light,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    progressIndicatorTheme: const ProgressIndicatorThemeData(
+      color: Constants.primaryColor400,
+      circularTrackColor: Constants.primaryColor600,
     ),
-    errorBorder: UnderlineInputBorder(
-      borderRadius: BorderRadius.circular(6),
-      borderSide: const BorderSide(
-        color: ConstantsV2.buttonPrimaryColor100,
-        width: 1.0,
-      ),
-    ),
-    errorStyle: bodySmallRegular.copyWith(
-      color: ConstantsV2.systemFail
-    )
-  ),
-  searchBarTheme: SearchBarThemeData(
-    elevation: MaterialStateProperty.all<double>(0),
-    backgroundColor: MaterialStateProperty.resolveWith((states){
-      switch (states.firstOrNull) {
-        case MaterialState.selected:
-        case MaterialState.pressed:
-        case MaterialState.focused:
-          return ConstantsV2.gray.withOpacity(0.5);
-        default:
-          return ConstantsV2.BGNeutral;
-      }
-    }),
-  ),
-  primaryColor: Colors.white,
-  scaffoldBackgroundColor: ConstantsV2.BGNeutral,
-  brightness: Brightness.light,
-  visualDensity: VisualDensity.adaptivePlatformDensity,
-  progressIndicatorTheme: const ProgressIndicatorThemeData(
-    color: Constants.primaryColor400,
-    circularTrackColor: Constants.primaryColor600,
-  ),
-  appBarTheme: const AppBarTheme()
-);
+    appBarTheme: const AppBarTheme());
 
 ButtonStyle elevatedButtonStyleSecondary = ButtonStyle(
   textStyle: MaterialStateProperty.all(
-    const TextStyle(fontWeight: FontWeight.w500, fontSize: 16,
-        color: ConstantsV2.BGNeutral
-    ),
+    const TextStyle(
+        fontWeight: FontWeight.w500,
+        fontSize: 16,
+        color: ConstantsV2.BGNeutral),
   ),
   padding: MaterialStateProperty.all(
     const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
@@ -687,7 +683,8 @@ class ConstantsV2 {
   static Color primaryLightAndClear = const Color(0xff28B3BB).withOpacity(.13);
 
   static const Color secondaryRegular = Color(0xffEB8B76);
-  static Color secondaryLightAndClear = const Color(0xffEB8B76).withOpacity(0.1);
+  static Color secondaryLightAndClear =
+      const Color(0xffEB8B76).withOpacity(0.1);
 
   static const Color systemSuccess = Color(0xff4AB648);
   static const Color systemFail = Color(0xffC53030);
@@ -711,9 +708,8 @@ class ConstantsV2 {
   static double homeFeedTitleContainerMinHeight = 32.0;
   static double homeCarouselPadding = 24.0;
 
-  static double homeExpandedMaxHeight = homeAppBarMaxHeight ;
-  static double homeExpandedMinHeight = homeAppBarMinHeight ;
-
+  static double homeExpandedMaxHeight = homeAppBarMaxHeight;
+  static double homeExpandedMinHeight = homeAppBarMinHeight;
 }
 
 // Constants
@@ -726,7 +722,9 @@ const String dependentSuccessAdded = '¡Dependiente agregado!';
 const String cancelActionMessage = 'Acción cancelada';
 DateTime minDate = DateTime(1900, 1, 1);
 DateTime minDateDigit = DateTime(1000, 1, 1);
-enum StatusRequestOrganization  {Approved, Pending, Rejected}
+
+enum StatusRequestOrganization { Approved, Pending, Rejected }
+
 const int timeToShowAppointmentsOnHoldInMonth = 1;
 const int timeToShowStudyOrderInMonth = 2;
 const Duration appearWidgetDuration = Duration(seconds: 1);
