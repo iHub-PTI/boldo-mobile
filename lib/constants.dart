fb5e9009823da45c77e48f804939cd88ca0b3819
Gabriel <maldoangel@hotmail.com>
2024-04-18 11:25:40 -0400

    fix: agregar sombra al appbar (debajo del logo de boldo)

--- lib/constants.dart
+++ lib/constants.dart
@@ -543,7 +543,7 @@ ThemeData boldoTheme = ThemeData(
     popupMenuTheme:
         const PopupMenuThemeData(surfaceTintColor: ConstantsV2.primaryColor),
     cardTheme: const CardTheme(surfaceTintColor: ConstantsV2.primaryColor),
-    appBarTheme: const AppBarTheme());
+    appBarTheme: const AppBarTheme(elevation: 6, shadowColor: Colors.black));
 
 ButtonStyle elevatedButtonStyleSecondary = ButtonStyle(
   textStyle: MaterialStateProperty.all(
