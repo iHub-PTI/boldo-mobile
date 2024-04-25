368276844b9d98e21cba93c1389c7d4af60deda6
everfgmolinas <everfgmolinas@hotmail.com>
2024-04-25 14:46:33 -0400

    fix: await finalize context use to pop context

--- lib/screens/my_studies/attach_files.dart
+++ lib/screens/my_studies/attach_files.dart
@@ -54,12 +54,12 @@ class _AttachFilesState extends State<AttachFiles> {
         body: SafeArea(
           child: BlocListener<NewStudyBloc, NewStudyState>(
             bloc: widget.newStudyBloc,
-            listener: (context, state) {
+            listener: (context, state) async {
               if (state is Uploaded) {
-                emitSnackBar(
+                await emitSnackBar(
                     context: context,
                     text: uploadedStudySuccessfullyMessage,
-                    status: ActionStatus.Success
+                  status: ActionStatus.Success,
                 );
                 Navigator.of(context)
                     .popUntil(ModalRoute.withName("/my_studies"));
