5a03b524709db466701bcce6f8685dfff9cb88cd
Gabriel <maldoangel@hotmail.com>
2024-05-07 10:04:51 -0400

    arreglar texto y agregar botón a Eliminar cuenta

--- lib/screens/my_account/delete_account_screen.dart
+++ lib/screens/my_account/delete_account_screen.dart
@@ -1,10 +1,6 @@
 import 'package:boldo/constants.dart';
-import 'package:boldo/screens/dashboard/tabs/components/item_menu.dart';
-import 'package:boldo/screens/privacy_policy/privacy_policy.dart';
-import 'package:boldo/screens/terms_of_services/terms_of_services.dart';
 import 'package:boldo/widgets/back_button.dart';
 import 'package:boldo/widgets/wrapper.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DeleteAccount extends StatefulWidget {
@@ -27,69 +23,82 @@ class _DeleteAccountState extends State<DeleteAccount> {
           margin: const EdgeInsets.only(top: 20),
           padding: const EdgeInsets.all(20),
           color: Colors.white,
-          child: Container(
-            alignment: Alignment.topLeft,
-            child: Column(children: [
-              const Text(
-                  'Lamentamos mucho saber que has decidido eliminar tu cuenta. Queremos asegurarnos de que este proceso se realice de manera segura y eficiente.'),
-              const SizedBox(
-                height: 25,
-              ),
-              RichText(
-                  text: TextSpan(
-                      style: boldoCorpSmallSTextStyle.copyWith(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                          color: ConstantsV2.activeText),
-                      children: [
-                    const TextSpan(
-                        text:
-                            'El proceso de eliminación de la cuenta tomará aproximadamente'),
-                    const TextSpan(
-                        style: TextStyle(color: ConstantsV2.orange),
-                        text: ' 30 días '),
-                    const TextSpan(
-                        text:
-                            'para completarse. Una vez finalizado este proceso, recibirás un correo electrónico de confirmación.')
-                  ])),
-              const SizedBox(
-                height: 25,
-              ),
-              const Text(
-                  'Durante este período, queremos informarte que los siguientes datos asociados a tu cuenta serán eliminados de nuestros sistemas:'),
-              const SizedBox(
-                height: 25,
-              ),
-              DefaultTextStyle(
-                  style: boldoCorpSmallSTextStyle.copyWith(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    color: ConstantsV2.activeText,
-                  ),
-                  child: const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+          child: Column(children: [
+            RichText(
+                text: TextSpan(
+                    style: boldoCorpSmallSTextStyle.copyWith(
+                        fontSize: 16, color: ConstantsV2.activeText),
                     children: [
-                      Text(
-                          '1. Información personal (nombre, dirección, fecha de nacimiento, etc.).'),
-                      Text('2. Datos de inicio de sesión y credenciales.'),
-                      Text('3. Historial de actividad y uso de la aplicación.'),
-                      Text(
-                          '4. Cualquier otra información relacionada específicamente con tu cuenta.'),
-                    ],
-                  )),
-              const SizedBox(
-                height: 25,
-              ),
-              const Text(
-                  'Por favor, ten en cuenta que esta acción es irreversible una vez completada. Si has reconsiderado esta decisión o necesitas alguna asistencia adicional, no dudes en contactarnos antes de que se complete el proceso de eliminación al correo info@bol.do')
-            ]),
-          ),
+                  const TextSpan(
+                      text:
+                          'Lamentamos mucho saber que has decidido eliminar tu cuenta. Queremos asegurarnos de que este proceso se realice de manera segura y eficiente.\n'),
+                  const WidgetSpan(
+                    child: SizedBox(
+                      height: 30,
+                    ),
+                  ),
+                  const TextSpan(
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                      ),
+                      children: [
+                        TextSpan(
+                            text:
+                                'El proceso de eliminación de la cuenta tomará aproximadamente'),
+                        TextSpan(
+                            style: TextStyle(color: ConstantsV2.orange),
+                            text: ' 30 días '),
+                        TextSpan(
+                            text:
+                                'para completarse. Una vez finalizado este proceso, recibirás un correo electrónico de confirmación.\n')
+                      ]),
+                  const WidgetSpan(
+                    child: SizedBox(
+                      height: 30,
+                    ),
+                  ),
+                  const TextSpan(
+                      text:
+                          'Durante este período, queremos informarte que los siguientes datos asociados a tu cuenta serán eliminados de nuestros sistemas: \n'),
+                  const WidgetSpan(
+                    child: SizedBox(
+                      height: 30,
+                    ),
+                  ),
+                  const TextSpan(
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                      ),
+                      text:
+                          '1. Información personal (nombre, dirección, fecha de nacimiento, etc.).\n2. Datos de inicio de sesión y credenciales. \n3. Historial de actividad y uso de la aplicación. \n4. Cualquier otra información relacionada específicamente con tu cuenta. \n'),
+                  const WidgetSpan(
+                    child: SizedBox(
+                      height: 30,
+                    ),
+                  ),
+                  const TextSpan(
+                      text:
+                          'Por favor, ten en cuenta que esta acción es irreversible una vez completada. Si has reconsiderado esta decisión o necesitas alguna asistencia adicional, no dudes en contactarnos antes de que se complete el proceso de eliminación al correo '),
+                  const TextSpan(
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                      text: 'info@bol.do')
+                ])),
+          ]),
         ),
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+      Container(
+        padding: const EdgeInsets.all(10),
+        color: Colors.white,
+        child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Quiero eliminar mi cuenta'),
+          )
+        ]),
       )
     ]);
   }
-}
-
-Widget _buildItemList(BuildContext context, int index, List<ItemMenu> items) {
-  return items[index];
 }
