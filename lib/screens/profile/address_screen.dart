8e505121cf749ad802ba137f604ed22386fab6be
Gabriel <maldoangel@hotmail.com>
2024-05-08 13:49:52 -0400

    actualizar diseño de la pantalla dirección (editar perfil)

--- lib/screens/profile/address_screen.dart
+++ lib/screens/profile/address_screen.dart
@@ -1,5 +1,6 @@
 import 'package:boldo/main.dart';
 import 'package:boldo/screens/profile/actions/sharedActions.dart';
+import 'package:boldo/widgets/back_button.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -40,7 +41,8 @@ class _AddressScreenState extends State<AddressScreen> {
     });
     Map<String, String>? updateResponse = await updateProfile(context: context);
     Provider.of<UserProvider>(context, listen: false).updateProfileEditMessages(
-        updateResponse["successMessage"]??'', updateResponse["errorMessage"]??'');
+        updateResponse["successMessage"] ?? '',
+        updateResponse["errorMessage"] ?? '');
     setState(() {
       loading = false;
     });
@@ -52,94 +54,85 @@ class _AddressScreenState extends State<AddressScreen> {
     return CustomWrapper(
       children: [
         const SizedBox(height: 20),
-        TextButton.icon(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(
-            Icons.chevron_left_rounded,
-            size: 25,
-            color: Constants.extraColor400,
-          ),
-          label: Text(
-            'Dirección',
-            style: boldoHeadingTextStyle.copyWith(fontSize: 20),
-          ),
+        BackButtonLabel(
+          labelText: 'Dirección',
         ),
-        const SizedBox(height: 20),
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Form(
-            autovalidateMode:
-                _validate ? AutovalidateMode.always : AutovalidateMode.disabled,
-            key: _formKey,
-            child: Column(
-              children: [
-                CustomFormInput(
-                  initialValue: editingPatient.street,
-                  label: "Calle",
-                  secondaryLabel: "Opcional",
-                  onChanged: (String val) =>
-                    editingPatient.street = val,
-                ),
-                const SizedBox(height: 20),
-                CustomFormInput(
-                  initialValue: editingPatient.neighborhood,
-                  label: "Barrio",
-                  secondaryLabel: "Opcional",
-                  onChanged: (String val) =>
-                    editingPatient.neighborhood = val,
-                ),
-                const SizedBox(height: 20),
-                CustomFormInput(
-                      initialValue: editingPatient.city,
-                      label: "Ciudad",
-                      secondaryLabel: "Opcional",
-                      onChanged: (String val) =>
-                        editingPatient.city = val,
-                    ),
-                const SizedBox(height: 20),
-                CustomFormInput(
-                      initialValue: editingPatient.addressDescription,
-                      maxLines: 6,
-                      label: "Referencia",
-                      secondaryLabel: "Opcional",
-                      onChanged: (String val) =>
-                        editingPatient.addressDescription = val,
-                    ),
-                const SizedBox(height: 26),
-                SizedBox(
-                  child: Column(
-                    children: [
-                      if (userProvider.profileEditErrorMessage != null)
-                        Text(
-                          userProvider.profileEditErrorMessage??'',
-                          style: const TextStyle(
-                            fontSize: 14,
-                            color: Constants.otherColor100,
-                          ),
-                        ),
-                      if (userProvider.profileEditSuccessMessage != null)
-                        Text(
-                          userProvider.profileEditSuccessMessage??'',
-                          style: const TextStyle(
-                            fontSize: 14,
-                            color: Constants.primaryColor600,
-                          ),
-                        ),
-                    ],
+          padding: const EdgeInsets.all(20.0),
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Form(
+              autovalidateMode: _validate
+                  ? AutovalidateMode.always
+                  : AutovalidateMode.disabled,
+              key: _formKey,
+              child: Column(
+                children: [
+                  CustomFormInput(
+                    initialValue: editingPatient.street,
+                    label: "Calle",
+                    secondaryLabel: "Opcional",
+                    onChanged: (String val) => editingPatient.street = val,
                   ),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                CustomFormButton(
-                  loading: loading,
-                  text: "Guardar",
-                  actionCallback: _updateLocation,
-                ),
-                const SizedBox(height: 16),
-              ],
+                  const SizedBox(height: 20),
+                  CustomFormInput(
+                    initialValue: editingPatient.neighborhood,
+                    label: "Barrio",
+                    secondaryLabel: "Opcional",
+                    onChanged: (String val) =>
+                        editingPatient.neighborhood = val,
+                  ),
+                  const SizedBox(height: 20),
+                  CustomFormInput(
+                    initialValue: editingPatient.city,
+                    label: "Ciudad",
+                    secondaryLabel: "Opcional",
+                    onChanged: (String val) => editingPatient.city = val,
+                  ),
+                  const SizedBox(height: 20),
+                  CustomFormInput(
+                    initialValue: editingPatient.addressDescription,
+                    maxLines: 6,
+                    label: "Referencia",
+                    secondaryLabel: "Opcional",
+                    onChanged: (String val) =>
+                        editingPatient.addressDescription = val,
+                  ),
+                  const SizedBox(height: 26),
+                  SizedBox(
+                    child: Column(
+                      children: [
+                        if (userProvider.profileEditErrorMessage != null)
+                          Text(
+                            userProvider.profileEditErrorMessage ?? '',
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Constants.otherColor100,
+                            ),
+                          ),
+                        if (userProvider.profileEditSuccessMessage != null)
+                          Text(
+                            userProvider.profileEditSuccessMessage ?? '',
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Constants.primaryColor600,
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  CustomFormButton(
+                    loading: loading,
+                    text: "Guardar",
+                    actionCallback: _updateLocation,
+                  ),
+                  const SizedBox(height: 16),
+                ],
+              ),
             ),
           ),
         ),
