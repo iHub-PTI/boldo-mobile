d9427fe6dc392c752780175aa370d6f2bfe785f9
everfgmolinas <everfgmolinas@hotmail.com>
2024-05-10 07:24:34 -0400

    style: improve spacing and trailing

--- lib/screens/pharmacy/components/pharmacy_available_card.dart
+++ lib/screens/pharmacy/components/pharmacy_available_card.dart
@@ -5,29 +5,25 @@ import 'package:boldo/widgets/organization_photo.dart';
 import 'package:flutter/material.dart';
 
 class PharmacyAvailableCard extends StatelessWidget {
-
   final Organization organization;
 
   PharmacyAvailableCard({
     Key? key,
     required this.organization,
-  }): super(key: key);
+  }) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.only(top: 4),
-      decoration: BoxDecoration(
-        color: ConstantsV2.lightest,
-        boxShadow: [
-          shadowRegular,
-        ]
-      ),
+      decoration: BoxDecoration(color: ConstantsV2.lightest, boxShadow: [
+        shadowRegular,
+      ]),
       child: Container(
         padding: const EdgeInsets.only(
-          top: 0, 
-          right: 8, 
-          bottom: 4, 
+          top: 0,
+          right: 8,
+          bottom: 4,
           left: 16,
         ),
         child: Row(
@@ -55,7 +51,7 @@ class PharmacyAvailableCard extends StatelessWidget {
     );
   }
 
-  Widget organizationDescription({required Organization organization}){
+  Widget organizationDescription({required Organization organization}) {
     return Container(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -66,7 +62,7 @@ class PharmacyAvailableCard extends StatelessWidget {
     );
   }
 
-  Widget _getFinancerHeader(){
+  Widget _getFinancerHeader() {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 8),
       decoration: const ShapeDecoration(
@@ -91,13 +87,13 @@ class PharmacyAvailableCard extends StatelessWidget {
     );
   }
 
-  Widget _getFinancerDescription(){
+  Widget _getFinancerDescription() {
     return Container(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            organization.name?? "Sin nombre",
+            organization.name ?? "Sin nombre",
             style: bodyLargeBlack.copyWith(
               color: ConstantsV2.activeText,
             ),
@@ -106,7 +102,7 @@ class PharmacyAvailableCard extends StatelessWidget {
             height: 6,
           ),
           Text(
-            organization.organzationName?? "Sin nombre",
+            organization.organizationName ?? "Sin nombre",
             style: regularText.copyWith(
               color: ConstantsV2.activeText,
               decoration: TextDecoration.underline,
@@ -120,7 +116,7 @@ class PharmacyAvailableCard extends StatelessWidget {
   Widget organizationDirection({
     required Organization organization,
     required BuildContext context,
-  }){
+  }) {
     return Container(
       padding: const EdgeInsets.only(right: 5, bottom: 5),
       child: Row(
@@ -149,36 +145,38 @@ class PharmacyAvailableCard extends StatelessWidget {
     );
   }
 
-  Widget _getAddress(){
+  Widget _getAddress() {
     return Text(
-      organization.address?.addressDescription?? "Av. Gral Santos 1254 casi Las Marias",
+      organization.address?.addressDescription ??
+          "Av. Gral Santos 1254 casi Las Marias",
       style: bodySmallRegular.copyWith(
         color: ConstantsV2.activeText,
       ),
     );
   }
 
-  Widget _getContact(){
+  Widget _getContact() {
+    //get phone type
+    Contact? _contactDescription = organization.contactList?.firstWhere(
+        (element) => element.type == 'phone',
+        orElse: () => organization.contactList!.first);
 
-      //get phone type
-      Contact? _contactDescription = organization.contactList?.firstWhere((element) => element.type == 'phone', orElse: ()=> organization.contactList!.first);
-
-      //get other contact type
-      _contactDescription = _contactDescription?? organization.contactList?.first;
-
+    //get other contact type
+    _contactDescription =
+        _contactDescription ?? organization.contactList?.first;
 
     return Row(
       children: [
-        if(_contactDescription != null)
-        _contactDescription.typeIcon(
-          color: ConstantsV2.orange,
-        ),
-        if(_contactDescription != null)
-        const SizedBox(
-          width: 5,
-        ),
+        if (_contactDescription != null)
+          _contactDescription.typeIcon(
+            color: ConstantsV2.orange,
+          ),
+        if (_contactDescription != null)
+          const SizedBox(
+            width: 5,
+          ),
         Text(
-          _contactDescription?.value?? "sin contacto",
+          _contactDescription?.value ?? "sin contacto",
           style: bodySmallRegular.copyWith(
             color: ConstantsV2.activeText,
           ),
@@ -186,5 +184,4 @@ class PharmacyAvailableCard extends StatelessWidget {
       ],
     );
   }
-
 }
