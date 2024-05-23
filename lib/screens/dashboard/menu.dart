5b0c43e8bb98c53cbe079b9bcb0929070450b94c
Gabriel <maldoangel@hotmail.com>
2024-05-23 11:29:56 -0400

    mejorar gradiente de menu

--- lib/screens/dashboard/menu.dart
+++ lib/screens/dashboard/menu.dart
@@ -82,6 +82,7 @@ class _MenuScreenState extends State<MenuScreen> {
   @override
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
     final double gradientHeight = screenHeight * 0.18;
     final double picturePadding = (screenHeight * 0.15) / 2;
     return BlocListener<UserLogoutBloc, UserLogoutState>(
@@ -105,17 +106,17 @@ class _MenuScreenState extends State<MenuScreen> {
             //const Background(text: "menu"),
             Container(
               width: double.infinity,
-              height: 122,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  // radius: MediaQuery.of(context).size.width / 180,
+              height: gradientHeight,
+              decoration: BoxDecoration(
+                gradient: RadialGradient(
+                  radius: screenWidth / 49,
                   colors: <Color>[
-                    ConstantsV2.patientAppBarColor300,
-                    ConstantsV2.patientAppBarColor200,
                     ConstantsV2.patientAppBarColor100,
+                    ConstantsV2.patientAppBarColor200,
+                    ConstantsV2.patientAppBarColor300,
                   ],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
+                  center: Alignment(1, screenWidth / 27.5),
+                  stops: [0.8, 0.85, 1.0],
                 ),
               ),
             ),
