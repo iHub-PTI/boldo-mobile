e97cf1aa8f8ecf9944a02e6526ced12bfa1b0700
Gabriel <maldoangel@hotmail.com>
2024-05-24 18:54:14 -0400

    corregir mayúsculas

--- lib/screens/family/tabs/family_register_account.dart
+++ lib/screens/family/tabs/family_register_account.dart
@@ -32,7 +32,7 @@ class _DniFamilyRegisterState extends State<DniFamilyRegister> {
   late var cameras;
   late var firstCamera;
   @override
-  void initState(){
+  void initState() {
     super.initState();
     initializedCamera();
   }
@@ -45,12 +45,12 @@ class _DniFamilyRegisterState extends State<DniFamilyRegister> {
 
   void getImageFromCamera() async {
     userImageSelected = await pickImage(
-        context: context,
-        source: ImageSource.camera,
-        maxWidth: 1000,
-        maxHeight: 1000,
-        imageQuality: 100,
-        permissionDescription: 'Se requiere acceso para tomar fotos'
+      context: context,
+      source: ImageSource.camera,
+      maxWidth: 1000,
+      maxHeight: 1000,
+      imageQuality: 100,
+      permissionDescription: 'Se requiere acceso para tomar fotos',
     );
     if (userImageSelected != null) {
       File? croppedFile = await cropPhoto(file: userImageSelected!);
@@ -58,21 +58,21 @@ class _DniFamilyRegisterState extends State<DniFamilyRegister> {
     }
     if (userImageSelected != null) {
       BlocProvider.of<PatientRegisterBloc>(context).add(
-          UploadPhoto(urlUploadType: photoStage, image: userImageSelected));
+        UploadPhoto(urlUploadType: photoStage, image: userImageSelected),
+      );
     } else {
       return;
     }
   }
 
   void getImageFromGallery() async {
-
     userImageSelected = await pickImage(
-        context: context,
-        source: ImageSource.gallery,
-        maxWidth: 1000,
-        maxHeight: 1000,
-        imageQuality: 100,
-        permissionDescription: 'Se requiere acceso para seleccionar una foto'
+      context: context,
+      source: ImageSource.gallery,
+      maxWidth: 1000,
+      maxHeight: 1000,
+      imageQuality: 100,
+      permissionDescription: 'Se requiere acceso para seleccionar una foto',
     );
     if (userImageSelected != null) {
       File? croppedFile = await cropPhoto(file: userImageSelected!);
@@ -80,7 +80,8 @@ class _DniFamilyRegisterState extends State<DniFamilyRegister> {
     }
     if (userImageSelected != null) {
       BlocProvider.of<PatientRegisterBloc>(context).add(
-          UploadPhoto(urlUploadType: photoStage, image: userImageSelected));
+        UploadPhoto(urlUploadType: photoStage, image: userImageSelected),
+      );
     } else {
       return;
     }
@@ -94,188 +95,208 @@ class _DniFamilyRegisterState extends State<DniFamilyRegister> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-            children: [
-              Container(
-                decoration: const BoxDecoration( // Background linear gradient
-                    gradient: LinearGradient(
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.topCenter,
-                        colors: <Color>[
-                          ConstantsV2.primaryColor100,
-                          ConstantsV2.primaryColor200,
-                          ConstantsV2.primaryColor300,
-                        ],
-                        stops: <double>[
-                          ConstantsV2.primaryStop100,
-                          ConstantsV2.primaryStop200,
-                          ConstantsV2.primaryStop300,
-                        ]
-                    )
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              // Background linear gradient
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: <Color>[
+                  ConstantsV2.primaryColor100,
+                  ConstantsV2.primaryColor200,
+                  ConstantsV2.primaryColor300,
+                ],
+                stops: <double>[
+                  ConstantsV2.primaryStop100,
+                  ConstantsV2.primaryStop200,
+                  ConstantsV2.primaryStop300,
+                ],
+              ),
+            ),
+          ),
+          Opacity(
+            opacity: 0.2,
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/register_background.png'),
                 ),
               ),
-              Opacity(
-                opacity: 0.2,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage(
-                            'assets/images/register_background.png')
-                    ),
-                  ),
-                ),
-              ),
-              BlocListener(
-                bloc: BlocProvider.of<PatientRegisterBloc>(context),
-                listener: (context, state) {
-                  if (state is SuccessPhotoUploaded) {
-                    setState(() {
-                    });
-                  }
-                  if (state is Failed) {
-                    emitSnackBar(
-                        context: context,
-                        text: state.response,
-                        status: ActionStatus.Fail
-                    );
-                  }
-                  if (state is NavigateNextScreen) {
-                    Navigator.pushNamed(context, state.routeName);
-                  }
-                  if (state is NavigateNextAndDeleteUntilScreen) {
-                    Navigator.of(context).pushNamedAndRemoveUntil(state.routeName, ModalRoute.withName(state.untilName) );
-                  }
-                },
-                child: BlocBuilder(
-                  bloc: BlocProvider.of<PatientRegisterBloc>(context),
-                  builder: (context, state) {
-                  if (state is Success) {
-                    return SuccesLoaded();
-                  }
-                  if (state is Loading) {
-                    return const LoadingHelper();
-                  }
-                    return SafeArea(
-                      child: Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: SingleChildScrollView(
-                          child: Column(
-                            children: [
-                              Container(
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    BackButtonLabel(
-                                      iconType: BackIcon.backClose,
-                                    ),
-                                  ],
+            ),
+          ),
+          BlocListener(
+            bloc: BlocProvider.of<PatientRegisterBloc>(context),
+            listener: (context, state) {
+              if (state is SuccessPhotoUploaded) {
+                setState(() {});
+              }
+              if (state is Failed) {
+                emitSnackBar(
+                  context: context,
+                  text: state.response,
+                  status: ActionStatus.Fail,
+                );
+              }
+              if (state is NavigateNextScreen) {
+                Navigator.pushNamed(context, state.routeName);
+              }
+              if (state is NavigateNextAndDeleteUntilScreen) {
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                  state.routeName,
+                  ModalRoute.withName(state.untilName),
+                );
+              }
+            },
+            child: BlocBuilder(
+              bloc: BlocProvider.of<PatientRegisterBloc>(context),
+              builder: (context, state) {
+                if (state is Success) {
+                  return SuccesLoaded();
+                }
+                if (state is Loading) {
+                  return const LoadingHelper();
+                }
+                return SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                BackButtonLabel(
+                                  iconType: BackIcon.backClose,
                                 ),
-                              ),
-                              Container(
-                                  child: SvgPicture.asset(
-                                    'assets/icon/logo_text.svg',
-                                    height: 100,
-                                    width: 100,
-                                  )),
-                              const SizedBox(
-                                height: 40,
-                              ),
-                              Text(
-                                'verificá tu identidad',
-                                textAlign: TextAlign.center,
-                                style: boldoSubTextStyle.copyWith(
-                                  fontSize: 25,
-                                  color: ConstantsV2.activeText
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              Align(
-                                alignment:
-                                photoStage == UrlUploadType.selfie ? Alignment.centerLeft : Alignment.center,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(10.0),
-                                  child: Text(
-                                    photoStage == UrlUploadType.selfie
-                                        ? 'Por último, tomate una selfie.'
-                                        : photoStage == UrlUploadType.frontal
+                              ],
+                            ),
+                          ),
+                          Container(
+                            child: SvgPicture.asset(
+                              'assets/icon/logo_text.svg',
+                              height: 100,
+                              width: 100,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          Text(
+                            'Verificá tu identidad',
+                            textAlign: TextAlign.center,
+                            style: boldoSubTextStyle.copyWith(
+                              fontSize: 25,
+                              color: ConstantsV2.activeText,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Align(
+                            alignment: photoStage == UrlUploadType.selfie
+                                ? Alignment.centerLeft
+                                : Alignment.center,
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Text(
+                                photoStage == UrlUploadType.selfie
+                                    ? 'Por último, tomate una selfie.'
+                                    : photoStage == UrlUploadType.frontal
                                         ? 'A continuación, subí una foto de la cara frontal de tu cédula de identidad paraguaya.'
                                         : 'Genial! Ahora una foto de la cara posterior de tu cédula de identidad paraguaya.',
-                                    style: boldoSubTextStyle.copyWith(
-                                        color: ConstantsV2.activeText,
-                                        fontWeight: FontWeight.w400),
-                                  ),
+                                style: boldoSubTextStyle.copyWith(
+                                  color: ConstantsV2.activeText,
+                                  fontWeight: FontWeight.w400,
                                 ),
                               ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              const Text('¿por qué me solicitan esta información?',
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      decoration: TextDecoration.underline,
-                                      color: ConstantsV2.activeText)),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              Image.asset(
-                                photoStage == UrlUploadType.selfie
-                                    ? 'assets/images/selfie.png'
-                                    : photoStage == UrlUploadType.frontal
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          const Text(
+                            '¿por qué me solicitan esta información?',
+                            style: TextStyle(
+                              fontSize: 16,
+                              decoration: TextDecoration.underline,
+                              color: ConstantsV2.activeText,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Image.asset(
+                            photoStage == UrlUploadType.selfie
+                                ? 'assets/images/selfie.png'
+                                : photoStage == UrlUploadType.frontal
                                     ? 'assets/images/CI_Illustration.png'
                                     : 'assets/images/CI_Illustration_back.png',
-                                height: 250,
-                                width: MediaQuery.of(context).size.width,
-                              ),
-                              const SizedBox(
-                                height: 40,
-                              ),
-                              Align(
-                                alignment: Alignment.bottomRight,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    photoStage != UrlUploadType.selfie
-                                        ? Padding(
-                                      padding: const EdgeInsets.only(right: 10.0),
-                                      child: OutlinedButton(
-                                          child: Row(
-                                              children: [
-                                                const Text("archivo"),
-                                                SvgPicture.asset('assets/icon/photo-library.svg'),
-                                              ]
-                                          ),
-                                          onPressed: () async {
-                                            getImageFromGallery();
-                                          }),
-                                    )
-                                        : Container(),
-                                    ElevatedButton(
+                            height: 250,
+                            width: MediaQuery.of(context).size.width,
+                          ),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                if (photoStage != UrlUploadType.selfie)
+                                  Padding(
+                                    padding: const EdgeInsets.only(right: 10),
+                                    child: OutlinedButton(
                                       child: Row(
-                                          children: [
-                                            const Text("camara"),
-                                            SvgPicture.asset('assets/icon/camera.svg'),
-                                          ]
+                                        children: [
+                                          const Text('Archivo'),
+                                          const SizedBox(
+                                            width: 4,
+                                          ),
+                                          SvgPicture.asset(
+                                            'assets/icon/photo-library.svg',
+                                          ),
+                                        ],
                                       ),
                                       onPressed: () async {
-                                        getImageFromCamera();
+                                        getImageFromGallery();
                                       },
                                     ),
-                                  ],
+                                  )
+                                else
+                                  Container(),
+                                ElevatedButton(
+                                  child: Row(
+                                    children: [
+                                      const Text('Cámara'),
+                                      const SizedBox(
+                                        width: 4,
+                                      ),
+                                      SvgPicture.asset(
+                                        'assets/icon/camera.svg',
+                                      ),
+                                    ],
+                                  ),
+                                  onPressed: () async {
+                                    getImageFromCamera();
+                                  },
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
+                        ],
                       ),
-                    );
-                  }
-                ),
-              )
-            ]
-        )
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
